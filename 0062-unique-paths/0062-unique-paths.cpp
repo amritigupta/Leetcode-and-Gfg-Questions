7b8df7fd717477a0e336da93465c9911b3dd1cfa
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-03-30 02:18:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int f (int i, int j, vector<vector<int>> &dp){
+    /* int f (int i, int j, vector<vector<int>> &dp){
         if (i==0 && j==0){
             return 1;
         }
@@ -12,10 +12,27 @@ class Solution {
         int left = f(i,j-1,dp);
         
         return dp[i][j]=up+left;
-    }
+    } */
 public:
-    int uniquePaths(int m, int n) {
+    /* int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n,-1));
         return f(m-1,n-1, dp);
+    } */
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (i==0 && j==0){dp[0][0]=1;}
+                else{
+                    int up=0;
+                    int left=0;
+                    if (i>0) up = dp[i-1][j];
+                    if (j>0) left = dp[i][j-1];
+                    
+                    dp[i][j]= up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
