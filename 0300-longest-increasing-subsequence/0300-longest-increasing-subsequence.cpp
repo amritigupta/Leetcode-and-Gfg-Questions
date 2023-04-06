b4d08fe5d448f0f779942cef32e4185e523de93e
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-04-07 04:24:49 +0530

    Time: 363 ms (47.76%), Space: 10.4 MB (78.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -24,24 +24,44 @@ public:
     
     
     
-    // TABULATION
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1,0));
+//     // TABULATION
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n+1, vector<int> (n+1,0));
         
-        for (int ind=n-1; ind>=0; ind--){
-            for(int prevind = ind-1; prevind>=-1; prevind--){
-                int nottake = 0 + dp[ind+1][prevind+1];
+//         for (int ind=n-1; ind>=0; ind--){
+//             for(int prevind = ind-1; prevind>=-1; prevind--){
+//                 int nottake = 0 + dp[ind+1][prevind+1];
                 
-                int take =0;
-                if (prevind==-1 || nums[ind]>nums[prevind]){
-                    take = 1 + dp[ind+1][ind+1];
-                }
+//                 int take =0;
+//                 if (prevind==-1 || nums[ind]>nums[prevind]){
+//                     take = 1 + dp[ind+1][ind+1];
+//                 }
                 
-                dp[ind][prevind+1]= max(take, nottake);
-            }
-        }
-        return dp[0][-1+1];
-    }
+//                 dp[ind][prevind+1]= max(take, nottake);
+//             }
+//         }
+//         return dp[0][-1+1];
+//     }
     
+
+    
+     int lengthOfLIS(vector<int>& arr) {
+         int n = arr.size();
+         vector<int> dp(n,1);
+         int maxi=1;
+         for (int i=0; i<n ;i++){
+             for(int prev=0; prev<i; prev++){
+                 if(arr[prev]<arr[i]){
+                     dp[i]= max(dp[i], 1+dp[prev]);
+                 }
+             }
+             maxi= max(maxi,dp[i]);
+         }
+         return maxi;
+     }
+       ///// PRINTING LIS
+    
+    //one dp array & one hash arrap
+    // max(dp[i]) returns length of LIS
 };
