0430cec5761b2429359ceb368e8f7d6f531e7a55
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-04-07 18:04:37 +0530

    Time: 60 ms (41.63%), Space: 9 MB (20.31%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -4,14 +4,14 @@ public:
         // subset means no order
         // subsequence means in order
         int n= arr.size();
-        vector<int>dp(n,1) ,hash(n); // taking dp vector and the track vector;
+        vector<int>dp(n,1) ,hash(n,-1); // taking dp vector and the track vector;
 
         int maxi=1; // count the length of the  subset 
         int lastIndex=0; // the index where  our last element of the subset is store
         sort(arr.begin(), arr.end());
 // sort an array we will sorted because the question is asking only for subset 
         for( int i=0;i<n;i++){  
-            hash[i]=i;
+        
             for( int prev=0;prev<i;prev++){
                 if(arr[i]%arr[prev]==0 && 1+dp[prev]>dp[i]){
 // if it will divisible by that and the length of the subset is greater than that
@@ -24,13 +24,16 @@ public:
             lastIndex=i;
             }
         } 
+        
 // this whole part is to store the element in  temp and return it
 // now print the subset in an array
          vector<int> temp;
-         temp.push_back(arr[lastIndex]);
-         while(hash[lastIndex]!=lastIndex){
-             lastIndex= hash[lastIndex];
+     
+        
+         while(lastIndex!=-1){
+            
              temp.push_back(arr[lastIndex]);
+             lastIndex= hash[lastIndex];
          }
          return temp;
     }
