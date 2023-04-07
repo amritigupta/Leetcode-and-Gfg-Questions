bcadb44d3c5dd56d9f85639bcf2c73706659f7d6
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-04-08 04:33:03 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,77 +6,77 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    // int numIslands(vector<vector<char>>& grid) {
-    //     // Code here
-    //     int n = grid.size();
-    //     int m = grid[0].size();
-        
-    //     int drow[] = {0,-1,0,1,-1,-1,1,1};
-    //     int dcol[]= {-1, 0, 1,0,-1,1,-1,1};
-        
-    //     vector<vector<bool>> vis(n, vector<bool> (m, false));
-        
-    //     int cnt =0;
-    //     for(int i=0; i<n; i++){
-    //         for (int j=0; j<m ; j++)
-    //           if (!vis[i][j] && grid[i][j]=='1'){
-    //             cnt++;
-    //             queue<pair<int,int>> q;
-    //             q.push({i,j});
-    //             vis[i][j]=true;
-                
-    //             while(!q.empty()){
-    //                 int row = q.front().first;
-    //                 int col = q.front().second;
-    //                 q.pop();
-                    
-    //                 for(int k=0; k<8; k++){
-    //                     int nrow = row + drow[k];
-    //                     int ncol = col + dcol[k];
-                        
-    //                     if (!vis[nrow][ncol] && grid[nrow][ncol]=='1' && nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-    //                         q.push({nrow,ncol});
-    //                         vis[nrow][ncol]=true;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return cnt;
-        
-    // }
-
-    
     int numIslands(vector<vector<char>>& grid) {
+        // Code here
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> visited(n, vector<bool> (m, false));
-        int cnt = 0;
-        vector<vector<int>> directions {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1,1}, {1, -1}, {-1, -1}, {-1, 1}};
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == '1' && visited[i][j] == false){
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
-                    cnt++;
-                    visited[i][j] = true;
-                    while(q.empty() == false){
-                        auto curr = q.front();
-                        q.pop();
-                        for(auto &e : directions){
-                            int newX = curr.first + e[0];
-                            int newY = curr.second + e[1];
-                            if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == '1' && visited[newX][newY] == false){
-                                visited[newX][newY] = true;
-                                q.push({newX, newY});
-                            }
+        
+        int drow[] = {0,-1,0,1,-1,-1,1,1};
+        int dcol[]= {-1, 0, 1,0,-1,1,-1,1};
+        
+        vector<vector<bool>> vis(n, vector<bool> (m, false));
+        
+        int cnt =0;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<m ; j++)
+              if (!vis[i][j] && grid[i][j]=='1'){
+                cnt++;
+                queue<pair<int,int>> q;
+                q.push({i,j});
+                vis[i][j]=true;
+                
+                while(!q.empty()){
+                    int row = q.front().first;
+                    int col = q.front().second;
+                    q.pop();
+                    
+                    for(int k=0; k<8; k++){
+                        int nrow = row + drow[k];
+                        int ncol = col + dcol[k];
+                        
+                        if ( nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
+                            q.push({nrow,ncol});
+                            vis[nrow][ncol]=true;
                         }
                     }
                 }
             }
         }
         return cnt;
+        
     }
+
+    
+    // int numIslands(vector<vector<char>>& grid) {
+    //     int n = grid.size();
+    //     int m = grid[0].size();
+    //     vector<vector<bool>> visited(n, vector<bool> (m, false));
+    //     int cnt = 0;
+    //     vector<vector<int>> directions {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1,1}, {1, -1}, {-1, -1}, {-1, 1}};
+    //     for(int i = 0; i < n; i++){
+    //         for(int j = 0; j < m; j++){
+    //             if(grid[i][j] == '1' && visited[i][j] == false){
+    //                 queue<pair<int, int>> q;
+    //                 q.push({i, j});
+    //                 cnt++;
+    //                 visited[i][j] = true;
+    //                 while(q.empty() == false){
+    //                     auto curr = q.front();
+    //                     q.pop();
+    //                     for(auto &e : directions){
+    //                         int newX = curr.first + e[0];
+    //                         int newY = curr.second + e[1];
+    //                         if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == '1' && visited[newX][newY] == false){
+    //                             visited[newX][newY] = true;
+    //                             q.push({newX, newY});
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return cnt;
+    // }
 };
 
 //{ Driver Code Starts.
