288a1ca4ae3e34443518b16317efdba52da27a10
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-04-08 02:48:23 +0530

    Time: 23 ms (79.46%), Space: 15 MB (35.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -30,13 +30,67 @@ class Solution {
             }
         }
         
+        
         for (int i=1; i<=n; i++){
             if(!vis[i]){
-                dfs(i,vis,adj);
                 cnt++;
+                queue<int> q;
+                
+                // dfs(i,vis,adj);
+                // cnt++;
+                
+                q.push(i);
+                vis[i]=1;
+                
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    for (auto it: adj[node]){
+                        if (!vis[it]){
+                            vis[it]=1;
+                        q.push(it);}
+                    }
+                }
+                
             }
         }
         return cnt;
     }
+    
+//    int findCircleNum(vector<vector<int>>& isConnected) {
+// 		int V=isConnected.size();
+// 		vector<int>adjls[V];
+// 		for(int i=0; i<V; i++){
+// 			for(int j=0; j<V; j++){
+// 				if(isConnected[i][j]==1 && i!=j){
+// 					adjls[i].push_back(j);
+// 					adjls[j].push_back(i);
+// 				}
+// 			}
+// 		}
+       
+       
+// 		vector<int>v(V, 0); //visited array
+       
+// 		int cnt=0;
+// 		for(int i=0; i<V; i++){
+// 			if(v[i]==0){
+// 				cnt++;
+// 				queue<int>q;
+// 				q.push(i);
+// 				while(!q.empty()){
+// 					int ft=q.front();
+// 					q.pop();
+// 					for(auto it: adjls[ft]){
+// 						if(!v[it]){
+// 							v[it]=1;
+// 							q.push(it);
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 		return cnt;
+// 	}
     
 };
