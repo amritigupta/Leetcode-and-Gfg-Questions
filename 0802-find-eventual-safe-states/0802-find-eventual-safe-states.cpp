5ccf2fd22cf68d1f546fdaa2035c85d45c135f93
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-04-10 02:21:08 +0530

    Time: 204 ms (48.51%), Space: 61.7 MB (12.40%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,46 +1,88 @@
 class Solution {
-    bool dfs(int node, vector<int> &vis, vector<int> &pathvis, vector<vector<int>>& graph, vector<int> &check){
-        vis[node]=1;
-        pathvis[node]=1;
-        check[node]=0;
+    
+    //METHOD 1 : DETECT CYCLE USING DFS :: IF A NODE DOESNT HAVE CYCLE THEN IT IS SAFENODE 
+//     bool dfs(int node, vector<int> &vis, vector<int> &pathvis, vector<vector<int>>& graph){
+//         vis[node]=1;
+//         pathvis[node]=1;
+ 
         
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                if (dfs(it, vis, pathvis, graph,check)==true) {
-                    // check[node]=0;
-                    return true;
-                }
-            }
-            if(vis[it] && pathvis[it]){
-                // check[node]=0;
-                return true; //cycle
-            }
-        }
-        
-        // check[node]=1; //safe
-        pathvis[node]=0;
-        return false;
-    }
+//         for(auto it: graph[node]){
+//             if(!vis[it]){
+//                 if (dfs(it, vis, pathvis, graph)==true) {
+//                     return true; //cycle
+//                 }
+//             }
+//             if(vis[it] && pathvis[it]){
+//                 return true; //cycle
+//             }
+//         }
+
+//         pathvis[node]=0;
+//         return false;
+//     }
     
     
 public:
+//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+//         int V = graph.size();
+//         vector<int> vis(V,0), pathvis(V,0);
+//         vector<int> safenodes;
+        
+//         for(int i=0; i<V;i++){
+//             if(vis[i]==0){
+//                 dfs(i, vis, pathvis, graph);
+//             }
+//         }
+//         for(int i=0; i<V; i++){
+//             if(pathvis[i]==0){
+//                 safenodes.push_back(i);
+//             }
+//         }
+        
+//         return safenodes;
+//     }
+    
+    
+    // METHOD 2 : KAHN'S ALGO :: SAFENODES ARE THOSE WHICH HAVE OUTDEGREE=0 AND HAVE PATH TO THAT 0 OUTDEGREE NODE
+    // WE KNOW KAHN'S ALGO CAN BE USED TO FIND ALL THOSE NODES WITH INDEGREE = 0  AND ALL NODES IN PATH OF IT
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> vis(V,0), pathvis(V,0);
-        vector<int> safenodes;
-        vector<int> check(V,0);
-        
-        for(int i=0; i<V;i++){
-            if(vis[i]==0){
-                dfs(i, vis, pathvis, graph, check);
-            }
+    int n=graph.size();
+	
+    vector<int>indegree(n,0); // creating array to store the indegree
+	
+    vector<vector<int>>adjrev(n); // we will be reversing the edges so that we can apply toposort as toposort work on indegree.
+	
+    for(int i=0;i<n;i++){
+        for(auto it:graph[i]){
+            adjrev[it].push_back(i);
+            indegree[i]++;
         }
-        for(int i=0; i<V; i++){
-            if(pathvis[i]==0){
-                safenodes.push_back(i);
-            }
-        }
-        
-        return safenodes;
     }
+    
+    queue<int>q;
+    for(int i=0;i<n;i++){
+        if(indegree[i]==0)
+            q.push(i);
+    }
+    
+        
+
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adjrev[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        
+        sort(topo.begin(), topo.end());
+        return topo;
+     }
+
 };
