2ac325dd46b668e1d79ce516e5cc4ac558059074
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-05 16:56:49 +0530

    Time: 27 ms (17.89%), Space: 20.2 MB (50.47%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,33 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-// DIAMETER = LONGEST PATH BETWEEN 2 NODES
-// PATH DOESNT NEED TO PASS THROUGH ROOT NECESSARILY
-
 class Solution {
-    int findheight(TreeNode* root){
-        if (root == NULL) return 0;
+    int height(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
         
-        return 1+max(findheight(root->left), findheight(root->right));
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        return 1+max(lh,rh);
     }
 public:
     int maxi=0;
     
     int diameterOfBinaryTree(TreeNode* root) {
-        if (root == NULL) return 0;
+        if(root==NULL){
+            return 0;
+        }
+        int lh = height(root->left);
+        int rh = height(root->right);
         
-        int lh = findheight(root->left);
-        
-        int rh = findheight(root->right);
-        
-        maxi = max(maxi, lh+rh);
+        maxi=max(maxi, lh+rh);
         
         diameterOfBinaryTree(root->left);
         diameterOfBinaryTree(root->right);
         
         return maxi;
-        
     }
 };
