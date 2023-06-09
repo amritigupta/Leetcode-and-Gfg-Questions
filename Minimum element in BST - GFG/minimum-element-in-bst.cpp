fc4383a42d8a27a7eeafb5c8a639730f0a7055b2
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-09 20:27:40 +0530

    Added solution - LeetHub

--- Minimum element in BST - GFG/minimum-element-in-bst.cpp
+++ Minimum element in BST - GFG/minimum-element-in-bst.cpp
@@ -112,12 +112,10 @@ Node* newNode(int val) {
 }
 */
 int minValue(Node* root) {
-    // Code here
-    if(root == NULL){return -1;}
-    
-    while(root->left){
+     // Code here
+    if(!root) return -1;
+    while(root->left!=NULL){
         root=root->left;
     }
-    
     return root->data;
 }
