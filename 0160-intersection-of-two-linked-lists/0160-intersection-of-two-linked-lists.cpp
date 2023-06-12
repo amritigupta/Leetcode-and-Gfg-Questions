d24febc7bb8f795f6c1813c525c8b31e5041a33c
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-12 06:30:29 +0530

    Time: 53 ms (50.43%), Space: 14.7 MB (32.81%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,20 +6,49 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+// 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+// 		unordered_map<ListNode*, int> m;
+// 		while(headA != NULL){
+// 			m[headA]++;
+// 			headA = headA -> next;
+// 		}
+// 		while(headB != NULL){
+// 			if(m[headB] > 0){
+// 				return headB;
+// 			}
+// 			headB = headB -> next;
+// 		}
+// 		return NULL;
+// 	}
+// };
+
 class Solution {
 public:
-	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-		unordered_map<ListNode*, int> m;
-		while(headA != NULL){
-			m[headA]++;
-			headA = headA -> next;
-		}
-		while(headB != NULL){
-			if(m[headB] > 0){
-				return headB;
-			}
-			headB = headB -> next;
-		}
-		return NULL;
-	}
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL){
+            return NULL;
+        }
+        
+        ListNode *a = headA;        //a is pointing head of 1st list
+        ListNode *b = headB;        //b is pointing head of 1st list
+        
+        while(a!=b){
+            if(a==NULL){        //if a traverse whole list and reach end
+                a=headB;        // then transfer to 2nd list
+            }
+            else{
+                a = a->next;    // otherwise traverse
+            }
+            
+            if(b==NULL){
+                b=headA;
+            }
+            else{
+                b = b->next;
+            }
+        }
+        return a;   //return a or b because they pointing on same intersecting point
+    }
 };
