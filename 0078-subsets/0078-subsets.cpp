99a8febe4ddc45eef990c92a993eb2e62faa3de2
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-18 20:20:14 +0530

    Time: 6 ms (28.35%), Space: 7.1 MB (78.00%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,53 @@
+// //USING RECURSION
+// class Solution {
+//     vector<vector<int>> ans;
+    
+//     void sub(vector<int> &nums, int i, vector<int> temp)
+//     {
+//         if(i==nums.size())
+//         {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         //not taking nums[i]
+//         sub(nums, i+1, temp);
+        
+//         //taking nums[i]
+//         temp.push_back(nums[i]);
+//         sub(nums, i+1, temp);
+//     }
+    
+    
+//     public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<int> temp;       
+//         sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
+//         return ans;
+//     }
+// };
+
+  // Bit Manipulation
+// Time complexity : O(N*(2^N))
+// Space complexity : O(N*(2^N))
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int num = pow(2, n), i = 0;
+        
+        while(i<num)
+        {
+            vector<int> temp;
+            for(int j=0; j<n; j++)
+            {
+                if(i & 1<<j)
+                    temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+            i++;
+        }
+        
+        return ans;
+    }
+};
