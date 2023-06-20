c3baf49a67c56146a4d43ad4f7b03a1188c17abd
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-21 00:57:09 +0530

    Time: 3 ms (68.17%), Space: 6 MB (26.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,29 @@
 class Solution {
-    int f(int N, vector<int> &dp){
-        if(N < 2)
-            return N;
+//     int f(int N, vector<int> &dp){
+//         if(N < 2)
+//             return N;
           
-        if(dp[N]!=-1){
-            return dp[N];
-        }
+//         if(dp[N]!=-1){
+//             return dp[N];
+//         }
         
-        return dp[N] = f(N-1, dp) + f(N-2, dp);
-    }
+//         return dp[N] = f(N-1, dp) + f(N-2, dp);
+//     }
 public:
     int fib(int N) {
         vector<int> dp(N + 1, -1);
-        return f(N, dp);
+        // return f(N, dp);
+        if(N < 2){
+            return N;
+        }
+        
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2; i<=N ; i++){
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+        
+        return dp[N];
     }
 };
