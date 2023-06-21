ae97abf38cc7de550ad4cc15441905472df1ba56
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-22 04:49:12 +0530

    Time: 139 ms (69.83%), Space: 17.6 MB (6.93%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,28 +1,36 @@
 class Solution {
-public:
-    int f(int i, int j1, int j2, int n, int m, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
-        if (j1 < 0 || j2 < 0 || j1 >= m || j2 >= m) return -1e9;
-        if (i == n-1) {
-            if (j1 == j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
+    int f(int i, int j1, int j2, int m , int n , vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+        if(j1<0 || j2<0 || j1>=n || j2>=n){
+            return 0;
         }
-        if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
-        int maxi = -1e9;
-        for (int dj1=-1; dj1 <= 1; dj1++) {
-            for (int dj2=-1; dj2 <= 1; dj2++) {
-                int value = 0;
-                if (j1 == j2) value = grid[i][j1];
-                else value = grid[i][j1] + grid[i][j2];
-                value += f(i+1, j1+dj1, j2+dj2, n, m, grid, dp);
-                maxi = max(maxi, value);
+        if(i==m-1){
+            if(j1==j2) return dp[i][j1][j2] = grid[i][j1];
+            else return dp[i][j1][j2]= grid[i][j1]+grid[i][j2];
+        }
+        
+        if(dp[i][j1][j2] !=-1){
+            return dp[i][j1][j2];
+        }
+        
+        int maxi=INT_MIN;
+        for(int dj1=-1; dj1<=1; dj1++){
+            for(int dj2=-1; dj2<=1; dj2++){
+                if(j1==j2){
+                    maxi = max(maxi, grid[i][j1]+ f(i+1, j1+dj1, j2+dj2, m,n,grid,dp));
+                }
+                else{
+                    maxi = max(maxi, grid[i][j1] + grid[i][j2] + f(i+1, j1+dj1, j2+dj2, m,n,grid,dp));
+                }
             }
         }
+        
         return dp[i][j1][j2] = maxi;
     }
+public:
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
-        return f(0, 0, m-1, n, m, grid, dp);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1, vector<int> (n+1,-1))); 
+        return f(0, 0, n-1, m, n, grid, dp);
     }
 };
