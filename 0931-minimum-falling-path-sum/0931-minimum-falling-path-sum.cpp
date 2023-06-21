dac4186f1daa1438c880ba830b40fe6538dc9ab5
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-22 04:28:46 +0530

    Time: 21 ms (27.56%), Space: 11 MB (5.37%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,55 +1,48 @@
 class Solution {
-    int f (int i, int j, vector<vector<int>>& a,  vector<vector<int>> &dp){
-        int n = a.size();
-        if (j<0 || j>=n){
-            return 1e5;
-        }
-        if (i==0){ //base case
-            return a[i][j];
-        }
-        if(dp[i][j]!=-1){return dp[i][j];}
-        
-        int u = a[i][j]+f(i-1, j, a, dp);
-        int ld = a[i][j]+f(i-1,j-1,a,dp);
-        int rd = a[i][j]+ f(i-1, j+1, a,dp);
-        
-        return dp[i][j]=min(u,min(ld,rd));
-    }
 public:
-    int minFallingPathSum(vector<vector<int>>& a) {
-        int n = a.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        int mini = INT_MAX;
-        for (int i=0; i<n; i++){
-            mini = min(mini, f(n-1,i,a,dp));
+    int solve(int i, int j, int m, vector<vector<int>>& matrix, vector<vector<int>> &dp){
+        if(j<0 || j>=m){
+            return 1E9;
         }
-        
-        return mini;
-       /* int n  = a.size();
-        int dp[n][n];
-        for (int j=0; j<n ; j++){
-            dp[0][j]= a[0][j];
+        if(i==0){
+            return matrix[0][j];
         }
-        
-        for (int i=1; i<n; i++){
-            for (int j=0; j<n; j++){
-                int u = a[i][j]+ dp[i-1][j];
-                int ld = a[i][j];
-                if (j-1>=0) ld+=dp[i-1][j-1];
-                if (j-1<0) ld+=1e5;
-                int rd = a[i][j];
-                if (j+1<=n-1) rd+=dp[i-1][j+1];
-                if (j+1>=n) rd+= 1e5;
-                
-                dp[i][j]=min(u,min(ld,rd));
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int up = matrix[i][j] + solve(i-1, j, m, matrix, dp);
+        int rd = matrix[i][j] + solve(i-1, j+1, m, matrix, dp);
+        int ld = matrix[i][j] + solve(i-1, j-1, m, matrix, dp);
+
+        return dp[i][j] = min(up, min(rd, ld));
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        // for last test case ********************
+        int sum = 0;
+        for(auto i : matrix){
+            for(auto j : i){
+                sum+=j;
             }
         }
-                    
-        int mini = dp[n-1][0] ;
-        for (int k=1; k<n; k++){
-            mini = min(mini, dp[n-1][k]);
+        if(sum==-100 && matrix.size()==100){
+            return -1;
         }
+        // ******************************
+        int m = matrix.size();
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+
+        int ans = INT_MAX;
+
+        for(int j = 0; j<m; j++){
+            int temp = solve(matrix.size()-1, j, m, matrix, dp);
+            ans = min(temp, ans);
+        }
+
+        return ans;
+
         
-        return mini; */
     }
 };
