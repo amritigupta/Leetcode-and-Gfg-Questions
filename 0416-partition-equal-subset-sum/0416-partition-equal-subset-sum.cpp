61ef5b431c78581a813eee8d48896644effc2bed
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-22 05:58:45 +0530

    Time: 356 ms (57.27%), Space: 95.8 MB (21.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,36 +1,39 @@
 class Solution {
-bool f (int i, int k, vector<int> &arr, vector<vector<int>> &dp){
-
-    if (k==0){return true;}
-    if (i==0){return arr[0]==k;}
-
-    if (dp[i][k]!=-1){return dp[i][k];}
-
-
-    bool nottake = f(i-1,k,arr,dp);
-    bool take = false;
-    if (arr[i]<=k) take = f(i-1,k-arr[i],arr,dp);
-
-    return dp[i][k]= (take|nottake);
-}
-    
-bool subsetSumToK(int n, int k, vector<int> &arr) {
-    // Write your code here.
-    vector<vector<int>> dp(n, vector<int> (k+1,-1));
-
-    return f(n-1,k,arr,dp);
-}
-    
+    bool f(int ind, int target, vector<int>& nums, vector<vector<int>> &dp){
+        if(target==0){
+            return true;
+        }
+        if(ind==0){
+            return nums[0]==target;
+        }
+        
+        if(dp[ind][target]!=-1){
+            return dp[ind][target];
+        }
+        
+        bool notpick = f(ind-1, target, nums, dp);
+        bool pick = false;
+        if(nums[ind]<=target){
+            pick = f(ind-1, target-nums[ind], nums, dp);
+        }
+        
+        return dp[ind][target]= pick|notpick;
+    }
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum=0;
-        for (int i=0 ; i<n; i++){
-            sum+=nums[i];
+        
+        for(int i=0; i<n ; i++){
+            sum+= nums[i];
         }
-        if (sum%2==1){return false;}
-        else{
-            return subsetSumToK(n, sum/2, nums);
+        
+        if(sum%2==1){
+            return false;
         }
+        
+        int m = sum/2;
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return f(n-1, sum/2 , nums, dp);
     }
 };
