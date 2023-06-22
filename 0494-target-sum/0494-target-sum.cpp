2f667361970d18b6d940ea58b71d96e2b26651ca
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-22 21:20:00 +0530

    Time: 11 ms (87.40%), Space: 12.1 MB (38.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,50 +1,41 @@
 class Solution {
-    int f (vector<int> &nums, int target, int ind, vector<vector<int>> &dp){
-        if (ind==0){ // BASE CASES
-            
-            if (target==0 && nums[0]==0){
-                return 2; //pick or notpick
-            }
-            else if(target==0 && nums[0]!=0){
-                return 1; //notpick
-            }
-            else if(nums[0]!=0 && target==nums[0]){
-                return 1; //pick
-            }
-            else{
-                return 0;
-            }
-            
+    int f(int ind, int target, vector<int> &nums, vector<vector<int>> &dp){
+        if(ind==0){
+            if (target == 0 && nums[0]==0) return 2;
+            else if(target == 0 || nums[0] == target) return 1;
+            else return 0;
         }
         
-        if (dp[ind][target]!=-1){
+        if(dp[ind][target] !=-1){
             return dp[ind][target];
         }
         
-        int nottake = 0+f(nums,target,ind-1,dp);
-        int take = 0;
-        if (nums[ind]<=target){
-            take= f(nums, target-nums[ind], ind-1, dp);
+        int nottake = f(ind-1, target, nums, dp);
+        int take =0;
+        if(target-nums[ind]>=0){
+            take = f(ind-1, target-nums[ind], nums, dp);
         }
-        
-        return dp[ind][target]= take+nottake;
-        
-        
+        return dp[ind][target] = (take+nottake);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int totsum=0;
-        for (int i=0; i<n; i++){
-            totsum+=nums[i];
-        }
+        // divide into 2 subsets such that s1 - s2 = target 
         
-        if (totsum-target<0 || (totsum-target)%2){
+        // s1 - s2 = target
+        // totsum - 2s1 = target
+        // s1 = (totsum - target)/2
+        
+        int n = nums.size();
+        int totsum = 0;
+        for(auto it: nums){
+            totsum+= it;
+        }
+        if(totsum - target < 0 || (totsum-target)%2 == 1){
             return 0;
         }
-        int S = (totsum-target)/2;
-        vector<vector<int>> dp(n, vector<int> (S+1,-1));
+        int s = (totsum-target)/2;
+        vector<vector<int>> dp(n+1, vector<int>(s+1, -1));
         
-        return f(nums, S, n-1, dp);
+        return f(n-1, s, nums, dp);
     }
 };
