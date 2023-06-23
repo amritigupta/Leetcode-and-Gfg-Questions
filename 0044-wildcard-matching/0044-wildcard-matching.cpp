d2f47a3d7ee06bd1429442af90541eb4bc5949a9
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-24 02:04:09 +0530

    Time: 62 ms (69.24%), Space: 27.7 MB (29.84%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,31 +1,40 @@
 class Solution {
-
-    
-public:
-
-    bool solve(int ind1,int ind2,string &s,string &p,vector<vector<int>> &dp){
-        if(ind1<0 && ind2<0) return true;
-        if(ind2<0) return false;
-        if(ind1<0){
-            for(int i=0;i<=ind2;i++){
-                if(p[i]!='*') return false;
-            }
+    bool f(int i, int j, string &s, string & t, vector<vector<int>> &dp){
+        if(i<0 & j<0){
             return true;
         }
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        
-        if(s[ind1]==p[ind2] || p[ind2]=='?') return dp[ind1][ind2]=solve(ind1-1,ind2-1,s,p,dp);
-        
-        else if(p[ind2]=='*'){
-            return dp[ind1][ind2]=solve(ind1-1,ind2,s,p,dp) || solve(ind1,ind2-1,s,p,dp);
+        if(j<0){
+            return false;
+        }
+        if(i<0){
+            for(int i=0;i<=j;i++){
+                if(t[i]!='*') return false;
+            }
+            return true;            
+        }
+        if( dp[i][j] !=-1){
+            return  dp[i][j] ;
         }
         
-        return dp[ind1][ind2]=false;
+        if(s[i]==t[j]){
+            return  dp[i][j] =  f(i-1, j-1, s, t, dp);
+        }
+        if (s[i]!=t[j] && t[j]=='?'){ //matches one character
+            return dp[i][j] = f(i-1, j-1, s, t, dp);
+        }
+        if (s[i]!=t[j] && t[j]=='*'){ //matches one character
+            return  dp[i][j] =  f(i-1, j, s, t, dp) || f(i, j-1, s,t,dp);
+        }  
+        else{
+            return  dp[i][j] =  false;
+        }
     }
+public:
     bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,p,dp);
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        return f(n-1, m-1, s, p, dp);
     }
 };
