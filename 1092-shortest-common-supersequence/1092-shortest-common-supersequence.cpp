c1256b885d81980175b824163b08768cf068b0e9
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-23 12:00:47 +0530

    Time: 34 ms (55.30%), Space: 12.8 MB (80.11%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
 
 public:
@@ -6,15 +5,17 @@ public:
         int n = str1.size(), m = str2.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        // Getting LCS
+        // Getting LCS (fill the table)
 
         for(int i=0; i<=n; i++) dp[i][0] = 0;
         for (int j=0; j<=m; j++) dp[0][j] = 0;
 
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=m; j++) {
-                if (str1[i-1] == str2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = 0 + max(dp[i][j-1], dp[i-1][j]);
+                if (str1[i-1] == str2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j] = 0 + max(dp[i][j-1], dp[i-1][j]);
             }
         }
 
@@ -29,15 +30,16 @@ public:
                 i--;
                 j--;
             }
-            else if (dp[i][j-1] >  dp[i-1][j]) {
+            else if (dp[i][j-1] >  dp[i-1][j]) { //column move left
                 ans += str2[j-1];
                 j--;
             }
             else {
-                ans += str1[i-1];
+                ans += str1[i-1]; //row move up
                 i--;
             }
         }
+       
         while (i > 0) {
             ans += str1[i-1];
             i--;
@@ -46,6 +48,7 @@ public:
             ans += str2[j-1];
             j--;
         };
+       
         reverse(begin(ans), end(ans));
         return ans;
     }
