dfd5c407b2e4d46bdc310052530175908eaec218
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-24 01:47:00 +0530

    Time: 92 ms (10.85%), Space: 80.7 MB (14.77%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,38 @@
 class Solution {
-    int f (int i, int j, string &s1 , string &s2, vector<vector<int>> &dp){
-        if(i<0){return j+1;}
-        if(j<0){return i+1;}
+    int f (int i, int j, string s, string t, vector<vector<int>> &dp){
+        // base cases
         
-        if (dp[i][j]!=-1){
-            return dp[i][j];
-        }        
-        if (s1[i]==s2[j]){
-            return dp[i][j]=f(i-1, j-1, s1,s2,dp);
+        if(i<0 && j<0){ // if "" & ""
+            return 0;
         }
-
+        if(j<0){ //j is exhausted "abcd" && ""
+                 // u require (i+1) operations then
+            return i+1;
+        }
+        if(i<0){ //i is exhausted "" && "hor"
+                 //to convert we can make 3 insertions or 3 deletions
+            return j+1;
+        }
         
-        int insert = 1+ f(i, j-1, s1,s2,dp);
-        int replace = 1+ f(i-1, j-1,s1,s2,dp);
-        int deletion = 1 + f(i-1,j,s1,s2,dp);
+        if(dp[i][j]!=-1) return dp[i][j];
         
-        return dp[i][j]= min(insert,min(replace,deletion));
+        if(s[i]==t[j]){
+            return dp[i][j] = f(i-1, j-1, s,t, dp);
+        }
+        else{
+            int insert = 1 + f(i, j-1, s, t, dp);
+            int del =  1 + f(i-1, j, s , t, dp);
+            int replace = 1 + f(i-1,j-1,s,t,dp);
+            
+            return dp[i][j] = min(insert, min(del, replace));
+        }
     }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2, dp);
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return f(n-1, m-1, word1, word2, dp);
     }
 };
