d488c843c500de1951cb31b0c684d5503dacb2d9
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-06-25 03:05:39 +0530

    Time: 15 ms (78.25%), Space: 14.4 MB (45.28%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -7,7 +7,7 @@ int f(int ind,int buy, int cap ,int n ,vector<int>&v,vector<vector<vector<int>>>
     if(dp[ind][buy][cap] != -1 ) return dp[ind][buy][cap];
     if(buy){
         notpick = 0 + f(ind+1,1,cap,n,v,dp);
-        pick = v[ind] + f(ind+1,0,cap-1,n,v,dp);
+        pick = v[ind] + f(ind+1,0,cap-1,n,v,dp); //whenever u sell reduce cap
     }
     else{
         notpick = 0 + f(ind+1,0,cap,n,v,dp);
