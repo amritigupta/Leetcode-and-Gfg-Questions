0cdf40c4cb63977fe5e8039c5e3b660bc130e4cc
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-02 06:23:37 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (61.30%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,50 +1,61 @@
+class Solution {
+public:
+    // APPROACH 1
+
+    void perm(vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,int freq[]){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){
+                
+                ds.push_back(nums[i]);
+                freq[i]=1; //mark visited
+                perm(ds,nums,ans,freq);
+                freq[i]=0; //mark not visited
+                ds.pop_back();
+                
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int freq[nums.size()]; //visited array
+        
+        for(int i=0;i<nums.size();i++){
+            freq[i]=0;
+        }
+        
+        perm(ds,nums,ans,freq);
+        return ans;
+    }
+
+};
+
+//     // APPROACH 2
 // class Solution {
 // public:
-//     void perm(vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,int freq[]){
-//         if(ds.size()==nums.size()){
-//             ans.push_back(ds);
+    
+//     void perm(int index,vector<int>&nums,vector<vector<int>>&ans){
+//         if(index==nums.size()){
+//             ans.push_back(nums);
 //             return;
 //         }
-//         for(int i=0;i<nums.size();i++){
-//             if(!freq[i]){
-//                 ds.push_back(nums[i]);
-//                 freq[i]=1;
-//                 perm(ds,nums,ans,freq);
-//                 freq[i]=0;
-//                 ds.pop_back();
-//             }
+//         for(int i= index;i<nums.size();i++){
+//             swap(nums[index],nums[i]);
+//             perm(index+1,nums,ans);
+//             swap(nums[index],nums[i]);
+            
 //         }
 //     }
+    
 //     vector<vector<int>> permute(vector<int>& nums) {
 //         vector<vector<int>>ans;
-//         vector<int>ds;
-//         int freq[nums.size()];
-//         for(int i=0;i<nums.size();i++){
-//             freq[i]=0;
-//         }
-//         perm(ds,nums,ans,freq);
+//         perm(0,nums,ans);
 //         return ans;
 //     }
 // };
-
-
-class Solution {
-public:
-    void perm(int index,vector<int>&nums,vector<vector<int>>&ans){
-        if(index==nums.size()){
-            ans.push_back(nums);
-            return;
-        }
-        for(int i= index;i<nums.size();i++){
-            swap(nums[index],nums[i]);
-            perm(index+1,nums,ans);
-            swap(nums[index],nums[i]);
-            
-        }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        perm(0,nums,ans);
-        return ans;
-    }
-};
