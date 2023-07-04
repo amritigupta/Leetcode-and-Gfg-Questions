6c11b01dd70cebe0ada95b6547c921e116f8624f
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-05 04:15:10 +0530

    Time: 4 ms (36.33%), Space: 6.5 MB (33.00%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans; 
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
     
-vector<string> letterCombinations(string digits) {
-	if(digits == "") return ans;         // no combinations formed without any digits
-	helper(digits, 0, "");              
-	return ans;
-}
-    
-// recursive helper function
-void helper(string &digits, int i, string combi){	
-	if(i == size(digits)) {  // base condition - push when we have recursed for all digits
-		ans.push_back(combi);    
-		return;
-	}
-	// add and recurse for each character (chosen from current digit-charcter mapping)
-	for(auto &c : mappings[digits[i] - '2'])
-		helper(digits, i + 1, combi + c);
-}
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+		vector<string> result;
+        result.push_back("");
+        
+        for(auto digit: digits) {
+            vector<string> tmp;
+            for(auto candidate: pad[digit - '0']) {
+                for(auto s: result) {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            result.swap(tmp);
+        }
+        return result;
+    }
 };
