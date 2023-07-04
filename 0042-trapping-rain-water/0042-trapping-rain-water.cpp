52013f87566184d7149572cfb243fc1a9cfbf51f
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-05 04:10:07 +0530

    Time: 21 ms (31.93%), Space: 20.6 MB (17.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,7 +3,7 @@ public:
     int trap(vector<int>& height) {
         
         int n = height.size();
-        vector<int> leftMax(n), rightMax(n);
+        vector<int> leftMax(n,0), rightMax(n,0);
         
         for (int i = 1; i < n; ++i) 
             leftMax[i] = max(height[i-1], leftMax[i-1]);
@@ -12,10 +12,12 @@ public:
             rightMax[i] = max(height[i+1], rightMax[i+1]);
         
         int ans = 0;
+        
         for (int i = 0; i < n; ++i) {
             int waterLevel = min(leftMax[i], rightMax[i]);
             if (waterLevel >= height[i]) ans += waterLevel - height[i];
         }
+        
         return ans;
     }
 };
