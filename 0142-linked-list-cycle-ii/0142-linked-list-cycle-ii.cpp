3c2e8da5331af6b4c850e446f797d3b4fc24cdfd
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-06 04:35:16 +0530

    Time: 8 ms (86.39%), Space: 7.6 MB (45.44%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -20,13 +20,16 @@ ListNode *detectCycle(ListNode *head) {
     while (fast->next && fast->next->next) {
         slow = slow->next;
         fast = fast->next->next;
+        
         if (slow == fast) {                      // there is a cycle
             while(slow != entry) {               // found the entry location
                 slow  = slow->next;
                 entry = entry->next;
             }
-            return entry;
+            
+            return entry; //returns loop start point
         }
+        
     }
     return NULL;                                 // there has no cycle
 }
