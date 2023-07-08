42b18145026dc96a642a050023cd1816b27044d4
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-08 23:17:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (91.87%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return x;
+        
+        long first = 1, last = x;
+        while (first <= last) {
+            long mid = first + (last - first) / 2;
+            // mid * mid == x gives runtime error
+            if (mid*mid == x)
+                return mid;
+            else if (mid > x / mid) {
+                last = mid - 1;
+            }
+            else {
+                first = mid + 1;
+            }
+        }
+        
+        int ans = last;
+        return ans;
+    }
+};
