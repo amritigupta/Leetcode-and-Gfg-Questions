b929ae19f9e847afc40a96ede7628411e9a4dcca
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-11 17:32:29 +0530

    Added solution - LeetHub

--- Alien Dictionary - GFG/alien-dictionary.cpp
+++ Alien Dictionary - GFG/alien-dictionary.cpp
@@ -8,64 +8,59 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
+    vector<int> toposort(vector<vector<int>> &adj, int k){
+        
+        vector<int> topo;
+        queue<int> q;
+        vector<int> indegree(k,0);
+        for(int i=0; i<k; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        for(int i=0; i<k ; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            topo.push_back(node);
+            q.pop();
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        
+        return topo;
+    }
     
-    	vector<int> topoSort(int V, vector<int> adj[]) {
-	    int indegree[V]={0};
-	    for (int i=0; i< V; i++){
-	        for (auto it: adj[i]){
-	            indegree[it]++;
-	        }
-	    }
-	    queue<int>q;
-	    
-	    for (int i=0; i<V; i++){
-	        if (indegree[i]==0){
-	            q.push(i);
-	        }
-	    }
-	    
-	    vector<int> ans;
-	    while (!q.empty()){
-	        int node = q.front();
-	        q.pop();
-	        ans.push_back(node);
-	        for (auto it: adj[node]){
-	            indegree[it]--;
-	            if (indegree[it]==0){
-	                q.push(it);
-	            }
-	        }
-	    }
-	    
-	    return ans;
-    	}
-    	
     public:
     string findOrder(string dict[], int N, int K) {
         //code here
-        vector<int> adj[K];
-        for (int i=0; i<N-1; i++){
-            string s1= dict[i];
+        vector<vector<int>> adj(K);
+        for(int i=0; i<N-1; i++){
+            string s1 = dict[i];
             string s2 = dict[i+1];
-            
-            int len = min(s1.size(), s2.size());
-            
-            for (int ptr=0 ; ptr<len; ptr++){
-                if (s1[ptr]!=s2[ptr]){
+            int len=min(s1.length(), s2.length());
+            for(int ptr=0; ptr<len; ptr++){
+                if(s1[ptr]!=s2[ptr]){
                     adj[s1[ptr]-'a'].push_back(s2[ptr]-'a');
                     break;
                 }
             }
         }
         
-        //find toposort
-        vector<int> topo = topoSort(K, adj);
+        vector<int> topo = toposort(adj,K);
         string ans = "";
-        for (auto it: topo){
-            ans = ans + char(it+'a');
+        for(auto it: topo){
+            ans= ans+char(it+'a');
         }
         return ans;
-        
     }
 };
 
