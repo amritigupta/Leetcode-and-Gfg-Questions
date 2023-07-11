7f52a32d2e9b7b6573b8c059a3e51c29466c98d5
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-12 01:37:33 +0530

    Time: 80 ms (95.57%), Space: 30.2 MB (93.20%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -2,37 +2,39 @@ class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
         int mod = 1e9+7;
-        vector<vector<pair<int, int>>> graph(n);
+        vector<vector<pair<int, int>>> adj(n);
         for(auto &road: roads) {
-            graph[road[0]].push_back({road[1], road[2]});
-            graph[road[1]].push_back({road[0], road[2]});
+            adj[road[0]].push_back({road[1], road[2]});
+            adj[road[1]].push_back({road[0], road[2]});
         }
         
         vector<long long> distance(n, LONG_MAX);
         vector<int> path(n, 0);
         
         priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        
+        //push {time, node}
         pq.push({0, 0});
         distance[0] = 0;
         path[0] = 1;
         
         while(!pq.empty()) {
-            pair<long long, int> t = pq.top();
+            long long time = pq.top().first;
+            long long node = pq.top().second;
             pq.pop();
             
-            for(auto &nbr: graph[t.second]) {
-                long long vert = nbr.first;
-                long long edge = nbr.second;
-                
-                if(distance[vert] > distance[t.second] + edge) {
-                    distance[vert] = distance[t.second] + edge;
-                    pq.push({distance[vert], vert});
-                    path[vert] = path[t.second] %mod;
+            for(auto it: adj[node]){
+                long long adjnode = it.first;
+                long long edw = it.second;
+                if(edw + time < distance[adjnode]){
+                    distance[adjnode]=time+edw;
+                    pq.push({distance[adjnode], adjnode});
+                    path[adjnode]=path[node];
                 }
-                else if(distance[vert] == t.first + edge) {
-                    path[vert] += path[t.second];
-                    path[vert] %= mod;
-                }
+                else if(edw + time == distance[adjnode]){
+                    path[adjnode]+=path[node];
+                    path[adjnode]%=mod;
+                }                
             }
         }
         
