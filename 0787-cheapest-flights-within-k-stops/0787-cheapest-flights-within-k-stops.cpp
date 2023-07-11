682c7d829852773d282671184c5e73b8eb544d04
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-12 00:52:55 +0530

    Time: 25 ms (72.10%), Space: 13.7 MB (27.17%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,58 +1,43 @@
 class Solution {
-    typedef priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> MinHeap;
- 
-//Graph data structure, implemented with adjacency list
-class Graph{
 public:
-    vector<vector<pair<int,int>>> adj;
-    int nNodes;
-    void generateGraph(vector<vector<int>> &input, int n){
-        nNodes = n;
-        adj.resize(nNodes+1);
-        for(int i=0; i<input.size(); i++){
-            adj[input[i][0]].push_back({input[i][1], input[i][2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it: flights){
+            adj[it[0]].push_back({it[1], it[2]});
         }
-    }
-    
-    void clear(){
-        adj.clear();
-    }
-};
- 
-int dijkstraSingleTarget(Graph g, int source, int target, int k){
-    MinHeap minHeap;
-    vector<int> dist(g.nNodes+1, INT_MAX);
-    vector<int> stops(g.nNodes+1, INT_MAX);
-    
-    dist[source] = 0;
-    stops[source] = 0;
-    minHeap.push({0, source, 0});
-    
-    while(!minHeap.empty()){
-        auto [curDist, curNode, curStops] = minHeap.top();
-        minHeap.pop();
-        if(curNode == target) return curDist;
-        if(curStops == k+1) continue;
-        for(auto &[nextNode, nextWeight] : g.adj[curNode]){
-            int nextDist = curDist + nextWeight;
-            int nextStops = curStops + 1;
-            if(nextDist < dist[nextNode] || nextStops < stops[nextNode]){
-                dist[nextNode] = nextDist;
-                stops[nextNode] = nextStops;
-                minHeap.push({nextDist, nextNode, nextStops});
+        
+        queue<pair<int, pair<int,int>>> q;
+        q.push({0,{src,0}});
+        
+        vector<int> dist(n , 1e9);
+        dist[src]=0;
+        
+        while(!q.empty()){
+            int stops = q.front().first;
+            int node = q.front().second.first;
+            int cost = q.front().second.second;
+            q.pop();
+            
+            
+            if(stops > K){
+                continue;
+            }
+            for(auto it: adj[node]){
+                int adjnode = it.first;
+                int edw = it.second;
+                
+                if(cost + edw < dist[adjnode] && stops<=K){
+                    dist[adjnode]= cost + edw;
+                    q.push({stops+1, { adjnode,  dist[adjnode]}});
+                }
             }
         }
+        
+        if(dist[dst]==1e9){
+            return -1;
+        }
+        return dist[dst];
+        
     }
-    return -1;
-}
- 
- 
-
-public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        Graph g;
-        g.generateGraph(flights, n);
-        return dijkstraSingleTarget(g, src, dst, k);
-    }
- 
 };
