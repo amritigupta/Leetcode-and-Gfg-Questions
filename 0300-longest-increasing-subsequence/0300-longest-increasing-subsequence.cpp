ad5845771e3eec9d582c96950cf251617c966a69
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-14 05:20:41 +0530

    Time: 1644 ms (11.42%), Space: 292.3 MB (8.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,54 +1,48 @@
 class Solution {
     
-//     int f (int ind, int prevind , vector<int> &nums, int &n, vector<vector<int>> &dp){
-//         if (ind == n){
-//             return 0;
-//         }
-//         if (dp[ind][prevind+1]!=-1){
-//             return dp[ind][prevind+1];
-//         }
-            
-//         //prevind remains same
-//         int nottake = 0 + f(ind+1, prevind, nums , n,dp);
-        
-//         int take = 0;
-        
-//         //why not if(ind==0)
-//         // cuz first element can be ANY index not just 0th
-        
-//         if ( prevind == -1 || nums[ind] > nums[prevind]) 
-//             //prevind is now changed to ind
-//             take = 1 + f(ind+1, ind, nums, n,dp);
-        
-//         return dp[ind][prevind+1]= max(take, nottake);
-        
-//     }
+int f (int ind, int prevind , vector<int> &nums, int &n, vector<vector<int>> &dp){
+     if(ind==n){
+         return 0;
+     }
+     
+     if(dp[ind][prevind+1]!=-1){
+         return dp[ind][prevind+1];
+     }
+     
+     int len = 0+f(ind+1, prevind, nums, n, dp);
+     
+     if(prevind==-1 || nums[prevind]<nums[ind]){
+         len = max(len, 1+f(ind+1, ind, nums, n, dp));
+     }
+     
+     return dp[ind][prevind+1] = len;           
+}
     
 public:
-//     int lengthOfLIS(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<vector<int>> dp(n, vector<int> (n+1,-1));
-//         return f(0,-1,nums,n,dp);
-//     }
+int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n+1,-1));
+        return f(0,-1,nums,n,dp);
+  }
     
     
-    // TABULATION
-    int lengthOfLIS(vector<int>& arr) {
-        int maxi=0;
-        int n = arr.size();
-        vector<int> dp(n+1, 1);
+//     // A DIFFERENT SORT OF TABULATION  (INVOLVES LESS STEPS )
+//     int lengthOfLIS(vector<int>& arr) {
+//         int maxi=0;
+//         int n = arr.size();
+//         vector<int> dp(n+1, 1); //each element itself assigned 1 as it itself is LIS 
         
-        for(int i=0; i<n; i++){
-            for(int prev=0; prev<i; prev++){
-                if(arr[prev] < arr[i]){
-                    dp[i]= max(dp[i], 1+dp[prev]);
-               }
-            }
+//         for(int i=0; i<n; i++){
+//             for(int prev=0; prev<i; prev++){
+//                 if(arr[prev] < arr[i]){
+//                     dp[i]= max(dp[i], 1+dp[prev]);
+//                }
+//             }
             
-            maxi = max(dp[i] , maxi);
-        }
+//             maxi = max(dp[i] , maxi);
+//         }
         
-        return maxi;
-    }
+//         return maxi;
+//     }
     
 };
