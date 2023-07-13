780cd87370787f5713a914f3b464e7a874007aae
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-14 04:56:19 +0530

    Time: 56 ms (77.81%), Space: 27.7 MB (32.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,37 +3,42 @@ class Solution {
         if(i<0 & j<0){
             return true;
         }
-        if(j<0){
+        
+        if(j<0) 
             return false;
-        }
+        
         if(i<0){
-            for(int i=0;i<=j;i++){
-                if(t[i]!='*') return false;
-            }
-            return true;            
+            for(int k=0; k<=j ; k++){
+                if(t[k]!='*'){
+                    return false;
+                }
+            }           
+            return true;
         }
-        if( dp[i][j] !=-1){
-            return  dp[i][j] ;
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         
         if(s[i]==t[j]){
-            return  dp[i][j] =  f(i-1, j-1, s, t, dp);
+            return dp[i][j] =  f(i-1,j-1,s,t,dp);
         }
-        if (s[i]!=t[j] && t[j]=='?'){ //matches one character
-            return dp[i][j] = f(i-1, j-1, s, t, dp);
+        else if(t[j]=='?'){
+            return  dp[i][j] = f(i-1,j-1,s,t,dp);
         }
-        if (s[i]!=t[j] && t[j]=='*'){ //matches one character
-            return  dp[i][j] =  f(i-1, j, s, t, dp) || f(i, j-1, s,t,dp);
-        }  
-        else{
-            return  dp[i][j] =  false;
+        else if(t[j]=='*'){
+            // * represents nothing
+            // * represents 1 or more
+            return dp[i][j] = f(i, j-1, s,t , dp)||f(i-1,j,s,t,dp);
         }
+        
+        return dp[i][j] = false;
     }
 public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-
+        
         vector<vector<int>> dp(n, vector<int>(m,-1));
         return f(n-1, m-1, s, p, dp);
     }
