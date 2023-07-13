ab0f75dafc7f905d720b727ca18034ede5846b00
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-13 06:17:38 +0530

    Time: 309 ms (36.39%), Space: 22.7 MB (35.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,39 +1,50 @@
-class Solution {
-private: 
-    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-        if(i == j){
-            return dp[i][j] = true;
-        }
-        if(j-i == 1){
-            if(s[i] == s[j]){
-                return dp[i][j] = true;
-            }
-            else{
-                return dp[i][j] = false;
-            }
-        }
-        if(s[i] == s[j] && dp[i+1][j-1] == true){
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
-        }
-    }
+class Solution 
+{
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int startIndex = 0; int maxlen = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int g=0; g<n; g++){
-            for(int i=0, j=g; j<n; i++, j++){
-                solve(dp, i, j, s);
-                if(dp[i][j] == true){
-                    if(j-i+1 > maxlen){
-                        startIndex = i;
-                        maxlen = j-i+1;
+    string longestPalindrome(string s) 
+    {
+        int n=s.size(); 
+        
+        //dp[i][j] will be false if substring s[i..j] is not palindrome, Else true
+        vector<vector<bool>> dp(n, vector<bool> (n,false));
+        
+        //All substrings of length 1 are palindromes
+        for(int i=0; i<n; i++) dp[i][i]=true;
+        
+        int start=0, lenMax=1;
+        
+        // check for sub-string of length 2.
+        for(int i=0; i<n-1; i++) 
+        {
+            if(s[i]==s[i+1])
+            {
+                dp[i][i+1] = true;
+                start = i;
+                lenMax = 2;
+            }
+        }
+        
+        // Check for sub-string of length greater than 2.
+        for(int k=3; k<=n; k++)  // k is length of substring
+        {
+            for(int i=0; i<n-k+1; i++) // Fix the starting index
+            {
+                int j=i+k-1; // Get the ending index of substring from starting index i and length k
+                
+                // checking for sub-string from ith index to jth index if s[i+1] to s[j-1] is a palindrome
+                if(dp[i+1][j-1]==true && s[i]==s[j])
+                {
+                    dp[i][j] = true;
+                    
+                    if(k>lenMax)
+                    {
+                        lenMax = k;
+                        start=i;
                     }
                 }
             }
         }
-        return s.substr(startIndex, maxlen);
+        
+        return s.substr(start, lenMax); //print sub-string from start to lenMax
     }
 };
