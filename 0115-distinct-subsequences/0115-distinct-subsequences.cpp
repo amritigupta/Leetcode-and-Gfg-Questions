55c6af93380ac116e3083e333d9ffaa3aec0c667
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-13 18:04:52 +0530

    Time: 2963 ms (5.02%), Space: 420 MB (15.27%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int prime = 1e9+7;
 
-int countUtil(string s1, string s2, int ind1, int ind2,vector<vector<int>>& dp){
+ int countUtil(string s1, string s2, int ind1, int ind2,vector<vector<int>>& dp){
     if(ind2<0)
         return 1;
     if(ind1<0)
@@ -21,10 +21,11 @@ int countUtil(string s1, string s2, int ind1, int ind2,vector<vector<int>>& dp){
     else{
         return dp[ind1][ind2] = countUtil(s1,s2,ind1-1,ind2,dp);
     }
-}
+     
+     
+ }
 
-int numDistinct(string &t, string &s) {
-    // Write your code here.
+  int numDistinct(string &t, string &s) {
     int lt = t.length();
     int ls = s.length();
     
