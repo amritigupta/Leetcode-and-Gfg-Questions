74c67a225622889bbb73e2b4115f501ea1c34d44
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-15 10:25:49 +0530

    Time: 3 ms (68.20%), Space: 7.4 MB (97.54%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,65 +1,60 @@
-// class Solution {
-// public:
-//     // APPROACH 1
-
-//     void perm(vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,int freq[]){
-//         if(ds.size()==nums.size()){
-//             ans.push_back(ds);
-//             return;
-//         }
-        
-//         for(int i=0;i<nums.size();i++){
-//             if(!freq[i]){
-                
-//                 ds.push_back(nums[i]);
-//                 freq[i]=1; //mark visited
-//                 perm(ds,nums,ans,freq);
-//                 freq[i]=0; //mark not visited
-//                 ds.pop_back();
-                
-//             }
-//         }
-//     }
-
-//     vector<vector<int>> permute(vector<int>& nums) {
-//         vector<vector<int>>ans;
-//         vector<int>ds;
-//         int freq[nums.size()]; //visited array
-        
-//         for(int i=0;i<nums.size();i++){
-//             freq[i]=0;
-//         }
-        
-//         perm(ds,nums,ans,freq);
-//         return ans;
-//     }
-
-// };
-
 class Solution {
 public:
-    vector<vector<int>>v;
-    void solve(vector<int>&a,int idx)
-    {
-        if(idx >= a.size())
-        {
-            v.push_back(a);
+    // APPROACH 1
+
+    
+    void perm(vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,vector<int> &vis){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
             return;
         }
-        for(int i = idx; i < a.size(); i++)
-        {
-            swap(a[i],a[idx]);
-            solve(a,idx+1);
-            swap(a[i],a[idx]);
+        
+        for(int i=0;i<nums.size();i++){
+            if(!vis[i]){
+                ds.push_back(nums[i]);
+                vis[i]=1; //mark visited
+                perm(ds,nums,ans,vis);
+                vis[i]=0; //mark not visited
+                ds.pop_back();
+            }
         }
     }
 
-    vector<vector<int>> permute(vector<int>& a) {
-        vector<int>p;
-        solve(a,0);
-        return v;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        vector<int> vis(nums.size(), 0);
+        
+        perm(ds,nums,ans,vis);
+        return ans;
     }
+
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>>v;
+//     void solve(vector<int>&a,int idx)
+//     {
+//         if(idx >= a.size())
+//         {
+//             v.push_back(a);
+//             return;
+//         }
+//         for(int i = idx; i < a.size(); i++)
+//         {
+//             swap(a[i],a[idx]);
+//             solve(a,idx+1);
+//             swap(a[i],a[idx]);
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& a) {
+//         vector<int>p;
+//         solve(a,0);
+//         return v;
+//     }
+// };
 
 //     // APPROACH 2
 // class Solution {
