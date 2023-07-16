493b6e107c52ef61485bab2372aedace61bc5aa8
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-16 20:06:29 +0530

    Time: 108 ms (96.95%), Space: 37.3 MB (99.22%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -12,6 +12,7 @@ public:
         
         int c = connections.size();
         if (n-1 > c) return -1;
+        int extra=0;
         
         parent.resize(n);
         for (int i = 0; i < n; i++)
@@ -21,13 +22,19 @@ public:
             int x = find(connections[i][0]);
             int y = find(connections[i][1]);
             if (x != y) parent[y] = x;
+            if(x==y) extra++; //same parent 
         }
         
+        
+        // ans stores number of connected components 
         int ans = 0;
         for (int i = 0; i < n; i++)
             if (parent[i] == i)
                 ans++;
         
-        return ans-1;
+        if(extra>=ans-1) return ans-1;
+        else{
+            return -1;
+        }
     }
 };
