5c2f209edad63fabe916df5659db1f20bb7e9553
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-16 18:53:39 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,149 +3,41 @@
 using namespace std;
 
 // } Driver Code Ends
-
-
-class DisjointSet{
-    vector <int> rank, parent, size;
-public:
-    DisjointSet(int n){
-        rank.resize(n+1, 0 );
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i < n; i++)
-        {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUltimateParent(int node){
-        if (node == parent[node]){
-            return node;
-        }
-
-        return parent[node] = findUltimateParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v){
-        int ulp_u = findUltimateParent(u);
-        int ulp_v = findUltimateParent(v);
-
-        if(ulp_u==ulp_v){
-            return;
-        }
-
-        if(rank[ulp_u]< rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-
-        else if (rank[ulp_v]< rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-
-        else { //if same rank 
-            parent[ulp_v] = ulp_u; // v attached to u
-            rank[ulp_u]++; //larger one grows in size
-        }
-    }
-
-    void unionBySize(int u, int v){
-        int ulp_u = findUltimateParent(u);
-        int ulp_v = findUltimateParent(v);
-
-        if(ulp_u==ulp_v){
-            return;
-        }     
-
-        if(size[ulp_u]< size[ulp_v])  {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-
-};
-
-
 class Solution
-
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    // int spanningTree(int V, vector<vector<int>> adj[])
-    // {
-    //     // code here
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
+        pq.push({0,0});
+        vector<int> vis(V,0);
+        int sum=0;
         
-    //     //PRIMS ALGO
-        
-    //     // MIN HEAP {WT,NODE}
-    //     // VISITED ARRAY
-        
-    //     vector<int> vis(V, 0);
-    //     priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
-        
-    //     int sum=0; 
-    //     pq.push({0,0});
-    //     while(!pq.empty()){
-    //         auto it = pq.top();
-    //         pq.pop();
-    //         int node = it.second;
-    //         int wt = it.first;
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
             
-    //         if (vis[node]){
-    //             continue;
-    //         }
-    //         else{
-    //             //add it to the MST
-    //             vis[node]=1;
-    //             sum+=wt;
-    //             for(auto it: adj[node]){
-    //                 int adjnode = it[0];
-    //                 int edw = it[1];
-    //                 if(!vis[adjnode]){
-    //                     pq.push({edw, adjnode});
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return sum;
-    // }
-    
-    
-    // KRUSKAL'S ALGO
-      int spanningTree(int V, vector<vector<int>> adj[]){
-          vector<pair <int , pair <int,int>>> edges; 
-          for(int i=0; i<V; i++){
-              for(auto it: adj[i]){
-                  int adjnode = it[0];
-                  int wt = it[1];
-                  int node = i;
-                  
-                  edges.push_back({wt,{node, adjnode}});
-              }
-          }
-          sort(edges.begin(), edges.end());
-          DisjointSet ds(V);
-          
-          int mstwt=0;
-          for (auto it : edges){
-              int wt = it.first;
-              int u = it.second.first;
-              int v = it.second.second;
-              
-              if(ds.findUltimateParent(u) != ds.findUltimateParent(v)){
-                  mstwt+=wt;
-                  ds.unionBySize(u,v);
-              }
-          }
-          
-          return mstwt;
+            int node = it.second;
+            int wt = it.first;
+            if(vis[node]==1) continue;
+            
+            //add it to mst
+            vis[node]=1;
+            sum+=wt;
+            
+            for(auto it2: adj[node]){
+                int adjnode = it2[0];
+                int edw = it2[1];
+                if(!vis[adjnode]){
+                    pq.push({edw, adjnode});
+                }
+            }
+        }
+        
+        return sum;
+       
     }
-    
-    
 };
 
 //{ Driver Code Starts.
