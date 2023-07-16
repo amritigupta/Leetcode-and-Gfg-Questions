ee21a542b18821f6faff8cb1351f2ced8f3f17c1
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-16 20:13:06 +0530

    Time: 89 ms (78.47%), Space: 34.1 MB (64.98%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -3,10 +3,9 @@
 class DisjointSet{
     
 public:
-    vector <int> rank, parent,size;
+    vector <int> parent,size;
     
     DisjointSet(int n){
-        rank.resize(n+1, 0 );
         parent.resize(n + 1);
         size.resize(n + 1);
         for (int i = 0; i < n; i++)
@@ -22,28 +21,6 @@ public:
         }
 
         return parent[node] = findUltimateParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v){
-        int ulp_u = findUltimateParent(u);
-        int ulp_v = findUltimateParent(v);
-
-        if(ulp_u==ulp_v){
-            return;
-        }
-
-        if(rank[ulp_u]< rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-
-        else if (rank[ulp_v]< rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-
-        else { //if same rank 
-            parent[ulp_v] = ulp_u; // v attached to u
-            rank[ulp_u]++; //larger one grows in size
-        }
     }
 
     void unionBySize(int u, int v){
