59f6d7de19d4579b621ad31fbcca830389a10f3e
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-17 21:50:45 +0530

    Time: 19 ms (74.90%), Space: 7.9 MB (55.80%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        unordered_map<char, int> mp;
-        for (auto i: t) mp[i]++;
-        //Creating Variables
-        int ans = INT_MAX;
-        int i = 0, j = 0;
-        int count = mp.size();
-        int start = 0;
-        // starting to  solve
 
-        while (j < s.length())
-        {
-            // CALCULATION STEP (As Aditya's general format)
-            mp[s[j]]--;
-            if (mp[s[j]] == 0) count--;
-            // trying to minimize the window size after getting a candidate for answer
-            if (count == 0)
-            {
-                while (count == 0)
-                {
-                    if (ans > j - i + 1)
-                    {
-                        ans = j - i + 1; //updating the answer
-                        start = i;
-                    }
-
-                    mp[s[i]]++;
-                    if (mp[s[i]] > 0)
-                        count++;
-                    i++;
-                }
-            }
-            j++;
-        }
-        if(ans!= INT_MAX) 
-            return s.substr(start, ans);
-        else
-            return "";
-    }
+     string minWindow(string s, string t) {
+         unordered_map <char, int> mpp;
+         for(auto it: t){
+             mpp[it]++;
+         }
+         int count = mpp.size();
+         int i=0;
+         int j=0;
+         int ans = INT_MAX;
+         int start=0;
+         
+         while(j<s.length()){
+             mpp[s[j]]--;
+             
+             if(mpp[s[j]]==0){
+                 count--;
+             }
+             
+             if(count==0){
+               while(count==0){
+                 if(ans>j-i+1){
+                     ans = j-i+1;
+                     start = i;
+                 }
+                 
+                 mpp[s[i]]++;
+                 if(mpp[s[i]]>0)
+                     count++;
+             
+                 i++;
+               }
+             }
+             j++;
+         }
+         
+         if(ans!=INT_MAX)
+             return s.substr(start,ans);
+         else{
+             return "";
+         }
+     }
 };
