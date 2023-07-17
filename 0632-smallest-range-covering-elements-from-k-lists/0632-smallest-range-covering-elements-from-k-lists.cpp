d01949b201ca19149c03bbe97611dea751d3868a
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-17 18:37:12 +0530

    Time: 72 ms (38.73%), Space: 14.3 MB (99.77%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -6,16 +6,18 @@ public:
         int n = nums.size();
         priority_queue<pi, vector<pi>, greater<pi>> pq;
         int mn = INT_MAX, mx = INT_MIN, range = INT_MAX;
+        
         for (int i=0; i<n; i++) {
             pq.push({nums[i][0], {i, 0}});
             mn = min(mn, nums[i][0]);
             mx = max(mx, nums[i][0]);
         }
+        
         int a = mn, b = mx;
         while (!pq.empty()) {
             pi curr = pq.top();
             pq.pop();
-            //cout<<curr.first<<" "<<curr.second.first<<" "<<curr.second.second<<endl;
+
             if (curr.second.second + 1 < nums[curr.second.first].size()) {
                 int r = curr.second.first, c = curr.second.second + 1;
                 pq.push({nums[r][c], {r, c}});
