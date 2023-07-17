eef401d7abc0ba0bbdcf029c612ec4ec9418b5ce
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-18 04:43:28 +0530

    Time: 87 ms (59.27%), Space: 31.1 MB (21.62%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,67 +1,39 @@
 class Solution {
-    /* 3 2 2 4 1 4*/
-  
-    bool ispos(int capacity, int threshold, vector<int>& arr){
-        int ans =0;
-        int days=1;
-        int c = capacity;
-        for(int i = 0; i<arr.size(); i++){
-            if(arr[i]<=c){
-                if(i!=arr.size()-1){
-                c=c-arr[i];
-                }
-            }
-            else{
-                if(i!=arr.size()-1){
-                     days++;
-                     // cout<<i<<'\t'<<arr[i]<<"\t"<<c<<"\t"<<days<<endl;
-                     c=capacity;
-                     c=c-arr[i];
-                }
-            }
-            
-            if(i==arr.size()-1){
-                if(c>=arr[i]){
-                    continue;
-                }
-                else{    
-                    // cout<<arr[i]<<"\t"<<c<<"\t"<<days<<endl;
-                    days++;
-                }
-            }
-        }
-
-        if(days<=threshold){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        int l = *max_element(weights.begin(), weights.end());
-        int sum=0;
-        for(auto it: weights){
-            sum+=it;
-        }
-        int r = sum;
+    bool helper(int mid,vector<int>& weights,int days){
+        int count = 1, sum = 0;
         
-        int ans = -1;
+        for(int i = 0; i < weights.size(); i++) {
+            sum += weights[i];
+            if (sum > mid) {
+                count++;
+                sum = weights[i];
+            }
+        }
+        if (count <= days)
+            return true;
+        return false;
+    }
     
-     
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(ispos(mid, days, weights)){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int maxCapacity=0;
+        int n = weights.size();
         
-      
-        return ans;
+        for(int i = 0; i < n; i++)
+            maxCapacity += weights[i];
+        
+        int low = *max_element(weights.begin(), weights.end()), high = maxCapacity;
+        int result;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (helper(mid, weights, days)) {
+                result = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return result;
     }
 };
