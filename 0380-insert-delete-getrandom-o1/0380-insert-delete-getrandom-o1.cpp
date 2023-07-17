78c51c9cb48f6ebff8e521c2abda17a71385af8d
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-18 00:04:54 +0530

    Time: 241 ms (79.32%), Space: 96.9 MB (64.62%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -13,13 +13,24 @@ public:
     bool insert(int val) {
         if (m.find(val) != m.end()) return false;
         nums.emplace_back(val);
+        // m[val] = index
         m[val] = nums.size() - 1;
         return true;
     }
     
    
     bool remove(int val) {
-        if (m.find(val) == m.end()) return false;
+        if (m.find(val) == m.end()) 
+            return false;
+        // val = 20
+        // m[val]=1
+        // last = nums.back() ie = 50
+        // m[last]= m[val] // 50 -> 1
+        // nums[m[val]]=last // store at 1 index - 50
+        // nums.pop_back() delete 50 from nums (we already added it at 1 index so dont worry)
+        // m.erase(val) //erase 20 from ur map
+        
+        
         int last = nums.back();
         m[last] = m[val];
         nums[m[val]] = last;
