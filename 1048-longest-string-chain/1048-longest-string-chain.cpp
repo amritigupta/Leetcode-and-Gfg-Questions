b0f6aee2641c4a80243dc2492ddf8a37c42496a8
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-18 04:05:07 +0530

    Time: 2530 ms (10.07%), Space: 644.6 MB (6.83%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -2,9 +2,10 @@
 class Solution {
     public:
    bool checkpossible(string s1 ,string s2){
-        int s1s = s1.size();
+       int s1s = s1.size();
        int s2s = s2.size();
         if(s1s != s2s + 1) return false;
+       
         int first = 0;
         int second = 0;
         while(first < s1s){
@@ -24,13 +25,17 @@ class Solution {
     static bool cmp(string s1 ,string s2){
         return s1.size() < s2.size();
     }
+    
     int longestStrChain(vector<string>& arr) {
         int n = arr.size();
         sort(arr.begin(),arr.end(),cmp);
         vector<int> dp(n,1);
         int maxi = 1;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
+                // prev = arr[j]
+                // curr = arr[i]
                 if(checkpossible(arr[i] , arr[j]) && 1 + dp[j] > dp[i]){
                     dp[i] = dp[j] + 1;
                 }
