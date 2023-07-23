24207a904510fbd62567c1e57243da558a4b318b
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-24 04:18:45 +0530

    Time: 18 ms (94.68%), Space: 16.3 MB (27.62%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,54 @@
-class MinStack {
-	public:
-		stack<int>s,minKeeper;
+// USING O(N)+O(N)
+// class MinStack {
+// 	public:
+// 		stack<int>s,minKeeper;
     
-		void push(int val) {
-			s.push(val); // push val in stack
+// 		void push(int val) {
+// 			s.push(val); // push val in stack
 
-			if(minKeeper.size()==0||val<=minKeeper.top()){//push in when empty or lesser than current minElement
-				minKeeper.push(val);
-			}
-		}
+// 			if(minKeeper.size()==0||val<=minKeeper.top()){//push in when empty or lesser than current minElement
+// 				minKeeper.push(val);
+// 			}
+// 		}
 
-		void pop() {
-			if(minKeeper.top() == s.top())//pop when last element was min element 
-				minKeeper.pop();
-			s.pop();
-		}
+// 		void pop() {
+// 			if(minKeeper.top() == s.top())//pop when last element was min element 
+// 				minKeeper.pop();
+// 			s.pop();
+// 		}
 
-		int top() {//return top of stack
-			return s.top();
-		}
+// 		int top() {//return top of stack
+// 			return s.top();
+// 		}
 
-		int getMin() {//return top of minKeeper
-			return minKeeper.top();
-		}
-	};
+// 		int getMin() {//return top of minKeeper
+// 			return minKeeper.top();
+// 		}
+// 	};
+
+//Optimal Solution
+// Using o(n)
+
+class MinStack {
+public:
+    vector< pair<int,int> > s;
+	
+    MinStack() { }
+    
+    void push(int val) {
+        if(s.empty())
+            s.push_back({val,val});
+        else
+            s.push_back({val,min(s.back().second,val)});    
+    }
+    
+    void pop() { s.pop_back(); }
+    
+    int top() { return s.back().first; }
+    
+    int getMin() { return s.back().second; }
+};
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
