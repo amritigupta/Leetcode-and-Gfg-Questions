867b1ef0ceddb110005d6bc5dea93b45a9e6185d
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-23 21:39:11 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -13,12 +13,12 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-    stack<int>s;
+       stack<int>s;
        
        for(int i=0;i<n;i++)
         s.push(i);
         
-        while(s.size()>=2){
+        while(s.size()!=1){
             int p=s.top();
             s.pop();
             int q=s.top();
