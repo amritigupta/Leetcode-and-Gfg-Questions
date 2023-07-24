824fe33da0e2e9f4722977125076ff8fb70c9328
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-25 00:53:13 +0530

    Time: 23 ms (66.67%), Space: 12.6 MB (5.52%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,19 +1,88 @@
 class Solution {
-public:
-    long long subArrayRanges(vector<int>& A) {
-    long ans = 0;
-    int n = A.size();
-    for (int i = 0; i < n; i++)
-    {
-        int mx = A[i], mn = A[i];
-        for (int j = i + 1; j < n; j++)
-        {
-            mx = max(mx, A[j]);
-            mn = min(mn, A[j]);
-
-            ans += (mx - mn);
+    vector<int> prevgreaterelement(vector<int> &nums){
+        stack<int> st;
+        st.push(-1);
+        int n = nums.size();
+        vector<int> res;  
+        for(int i=0; i<n; i++){
+            while(st.top()!=-1 && nums[st.top()]<nums[i]){
+                st.pop();
+            }
+            res.push_back(st.top());
+            st.push(i);
         }
+        return res;        
     }
-    return ans;
-}
+    vector<int> nextgreaterelement(vector<int> &nums){
+        stack<int> st;
+        int n = nums.size();
+        st.push(n);
+        
+        
+        vector<int> res;    
+        for(int i=n-1; i>=0; i--){
+            while(st.top()!=n && nums[st.top()]<=nums[i]){
+                st.pop();
+            }
+            res.push_back(st.top());
+            st.push(i);
+        }
+        reverse(res.begin(), res.end());
+        return res;        
+    }
+    vector<int> nextsmallerelement(vector<int> &nums){
+        stack<int> st;
+        int n = nums.size();
+        st.push(n);
+      
+        vector<int> res;
+        for(int i=n-1; i>=0; i--){
+            while(st.top()!=n && nums[st.top()]>=nums[i]){
+                st.pop();
+            }
+            res.push_back(st.top());
+            st.push(i);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    vector<int> prevsmallerelement(vector<int> &nums){
+        stack<int> st;
+        st.push(-1);
+        int n = nums.size();
+        vector<int> res;
+        for(int i=0; i<n; i++){
+            while(st.top()!=-1 && nums[st.top()]>nums[i]){
+                st.pop();
+            }
+            res.push_back(st.top());
+            st.push(i);
+        }
+        return res;
+    }
+    
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>prevSmaller = prevsmallerelement(nums);
+        vector<int>prevGreater = prevgreaterelement(nums);
+        vector<int>nextSmaller = nextsmallerelement(nums);
+        vector<int>nextGreater = nextgreaterelement(nums);
+        
+        long long ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int prevRangeCount = i - prevSmaller[i];
+            int nextRangeCount = nextSmaller[i] - i;
+            long long minSubarrayCount = (long long)prevRangeCount * nextRangeCount;
+            ans = ans - (long long)(nums[i] * minSubarrayCount);
+            
+            int prevRangeCount1 = i - prevGreater[i];
+            int nextRangeCount1 = nextGreater[i] - i;
+            long long maxSubarrayCount = prevRangeCount1 * nextRangeCount1;
+            ans = ans + (long long)(nums[i] * maxSubarrayCount);
+        }
+        return ans;      
+        
+    }
 };
