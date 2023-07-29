a05c6577027d699a19ce39d32676953bb098e48f
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-29 20:30:05 +0530

    Time: 33 ms (99.78%), Space: 8.8 MB (57.22%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& arr) {
-        // subset means no order
-        // subsequence means in order
-        int n= arr.size();
-        vector<int>dp(n,1) ,hash(n,-1); // taking dp vector and the track vector;
-
-        int maxi=1; // count the length of the  subset 
-        int lastIndex=0; // the index where  our last element of the subset is store
-        sort(arr.begin(), arr.end());
-// sort an array we will sorted because the question is asking only for subset 
-        for( int i=0;i<n;i++){  
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         
-            for( int prev=0;prev<i;prev++){
-                if(arr[i]%arr[prev]==0 && 1+dp[prev]>dp[i]){
-// if it will divisible by that and the length of the subset is greater than that
+        int n = nums.size();
+ 
+        vector<int> dp(n, 1);
+        vector<int> hash(n);
+        for(int i=0; i<n ; i++){
+            hash[i]=i;
+        }
+        
+        int maxi=1;
+        int mxindex=0;
+        for(int i=1; i<n ; i++){
+            for(int prev = 0; prev<i; prev++){
+                if(nums[i]%nums[prev]==0 && 1+dp[prev]>dp[i]){
                     dp[i]=1+dp[prev];
                     hash[i]=prev;
                 }
             }
-            if(dp[i]>maxi){
-            maxi=dp[i];
-            lastIndex=i;
+            if(maxi < dp[i]){
+                maxi = dp[i];
+                mxindex= i;
             }
-        } 
+        }
+        vector<int> ans;
+        ans.push_back(nums[mxindex]);
+        while(mxindex!=hash[mxindex]){
+            mxindex = hash[mxindex];
+            ans.push_back(nums[mxindex]);
+        }
         
-// this whole part is to store the element in  temp and return it
-// now print the subset in an array
-         vector<int> temp;
-     
-        
-         while(lastIndex!=-1){
-            
-             temp.push_back(arr[lastIndex]);
-             lastIndex= hash[lastIndex];
-         }
-         return temp;
+        return ans;
     }
-
-    
 };
