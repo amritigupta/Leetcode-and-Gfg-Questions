aea9e7b51b4e64e9111c3b2a12d42a29e0124c73
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-29 20:38:12 +0530

    Time: 156 ms (78.01%), Space: 13.1 MB (64.17%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -16,13 +16,16 @@ public:
                 else if(nums[prev]<nums[i] && 1+dp[prev]==dp[i]){
                     cnt[i]+=cnt[prev];
                 }
+                
             }
             maxi = max(dp[i], maxi);
         }
+        
         int nos=0;
         for(int i=0; i<n; i++){
            if(dp[i]==maxi){nos+=cnt[i];}
         }
+        
         return nos;
     }
 };
