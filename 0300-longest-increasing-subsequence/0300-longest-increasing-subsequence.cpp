f6e295379bb01dbe751828b8454a320ab4053a28
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-07-29 19:12:40 +0530

    Time: 9 ms (91.02%), Space: 10.3 MB (84.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,29 @@
 class Solution {
+    //USING MEMOIZATION
+// int f (int ind, int prevind , vector<int> &nums, int &n, vector<vector<int>> &dp){
+//      if(ind==n){
+//          return 0;
+//      }
+     
+//      if(dp[ind][prevind+1]!=-1){
+//          return dp[ind][prevind+1];
+//      }
+     
+//      int len = 0+f(ind+1, prevind, nums, n, dp);
+     
+//      if(prevind==-1 || nums[prevind]<nums[ind]){
+//          len = max(len, 1+f(ind+1, ind, nums, n, dp));
+//      }
+     
+//      return dp[ind][prevind+1] = len;           
+// }
     
-int f (int ind, int prevind , vector<int> &nums, int &n, vector<vector<int>> &dp){
-     if(ind==n){
-         return 0;
-     }
-     
-     if(dp[ind][prevind+1]!=-1){
-         return dp[ind][prevind+1];
-     }
-     
-     int len = 0+f(ind+1, prevind, nums, n, dp);
-     
-     if(prevind==-1 || nums[prevind]<nums[ind]){
-         len = max(len, 1+f(ind+1, ind, nums, n, dp));
-     }
-     
-     return dp[ind][prevind+1] = len;           
-}
-    
-public:
-int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n+1,-1));
-        return f(0,-1,nums,n,dp);
-  }
+// public:
+// int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n, vector<int> (n+1,-1));
+//         return f(0,-1,nums,n,dp);
+//   }
     
     
 //     // A DIFFERENT SORT OF TABULATION  (INVOLVES LESS STEPS )
@@ -45,4 +45,21 @@ int lengthOfLIS(vector<int>& nums) {
 //         return maxi;
 //     }
     
+    // USING BINARY SEARCH
+    public:
+     int lengthOfLIS(vector<int>& arr) {
+         int n = arr.size();
+         vector<int> temp;
+         temp.push_back(arr[0]);
+         for(int i=1; i<n; i++){
+             if(arr[i]>temp.back()){
+                 temp.push_back(arr[i]);
+             }
+             else{
+                 int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                 temp[ind]=arr[i];
+             }
+         }
+         return temp.size();
+     }
 };
