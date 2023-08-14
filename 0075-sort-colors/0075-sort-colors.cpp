3fe17ffae9652a3488029f47e76726ab97613498
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-08-14 19:39:38 +0530

    Time: 4 ms (27.64%), Space: 8.2 MB (82.44%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,33 +1,5 @@
 class Solution {
 public:
-    // BETTER SOLUTION
-//     void sortColors(vector<int>& nums) { 
-
-//         map<int, int> mymap;
-//         int n= nums.size();
-
-//         for (int i=0; i<n; i++){
-//             mymap[nums[i]]++;
-//         }
-
-     
-//         int j=0;
-
-//         for (int i=0; i<mymap[0]; i++){
-//             nums[j++]=0;
-//         }
-
-//         for (int i=0; i<mymap[1]; i++){
-//             nums[j++]=1;
-//         }
-
-//         for (int i=0; i<mymap[2]; i++){
-//             nums[j++]=2;
-//         }
-
-//     } 
-    
-    // OPTIMAL SOLUTION
     void sortColors(vector<int>& arr) {
         int n = arr.size();
         int low=0;
