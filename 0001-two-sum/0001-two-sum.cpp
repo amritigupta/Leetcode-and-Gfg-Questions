9850236b753e737696c6a7d903cd27dc08cb906d
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-08-14 13:32:56 +0530

    Time: 8 ms (89.54%), Space: 12.5 MB (5.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,4 @@
 
-/*class Solution {
-public:
-    vector<int> twoSum(vector<int> a, int target) {
-        int i;
-        map<int,int>mymap;
-        map<int,int>::iterator it;
-        vector<int>ans;
-        for(i=0;i<a.size();i++){
-            it=mymap.find(target-a[i]);
-                if(it!=mymap.end()){
-                     ans.push_back(it->second);
-                     ans.push_back(i);
-                     break;
-                }
-            else{
-                mymap.insert(make_pair(a[i],i));
-            }
-        }
-        return ans;
-    }
-}; */
 
 class Solution {
 public:
