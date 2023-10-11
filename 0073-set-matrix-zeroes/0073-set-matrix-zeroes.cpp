c5c3ac0d7e3d659ff3b31cb3dda15e0b81b46a9d
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-10-11 22:44:23 +0530

    Time: 12 ms (55.41%), Space: 14 MB (11.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,59 +1,44 @@
 class Solution {
 public:
-    // O(M+N) SPACE COMPLEXITY
-    
-//     void setZeroes(vector<vector<int>>& matrix) {
-//         int n = matrix.size(); //nrows
-//         int m = matrix[0].size(); //ncols
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        int first_row0 = false, first_col0 = false;
+        //Consider first row and first columnas dummy row and column
+        //We use these dummy row and column to store the state of a row or column
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(matrix[i][j] == 0) {
+                    if(i == 0)
+                        first_row0 = true; //Checking whether entire dummy row would be zero
+                    if(j == 0)
+                        first_col0 = true; //Checking whether entire dummy column would be zero
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
         
-//         vector<int> row(n,0);
-//         vector<int> col(m,0);
+        //Traverse from (1,1) to (rows-1, cols-1) or vice-versa
+        //This would ensure we don not currently effect the dummy rows and columns
+        for(int i = rows - 1; i >= 1; i--) {
+            for(int j = cols - 1; j >= 1; j--) {
+                if(matrix[i][0] == 0 or matrix[0][j] == 0 )
+                    matrix[i][j] = 0;
+            }
+        }
         
-//         for(int i=0;i<n;i++){
-//             for(int j=0; j<m; j++){
-                
-//                 if (matrix[i][j]==0){
-//                     row[i]=1;
-//                     col[j]=1;
-//                 }
-                
-//             }
-//         }
-        
-//         for(int i=0;i<n;i++){
-//             for(int j=0; j<m; j++){
-                
-//                 if(row[i] == 1 || col[j] == 1){
-//                     matrix[i][j]=0;
-//                 }
-                
-//             }
-//         }        
-//     }
-    
-    
-    // 0{1} SPACE COMPLEXITY
-void setZeroes(vector<vector<int> > &matrix) {
-    int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-
-    for (int i = 0; i < rows; i++) {
-        //J=0
-        if (matrix[i][0] == 0) col0 = 0;
-        
-        //J!=0
-        for (int j = 1; j < cols; j++)
-            if (matrix[i][j] == 0)
-                matrix[i][0] = matrix[0][j] = 0;
+        //Now we handle the dummy row and column
+        if(first_row0 == true) {
+            //Make the entire first row zero
+            for(int i = 0; i < cols; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(first_col0 == true) {
+            //Make the entire first column zero
+            for(int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
-
-    for (int i = rows - 1; i >= 0; i--) {
-        //J!=0
-        for (int j = cols - 1; j >= 1; j--)
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                matrix[i][j] = 0;
-        
-        //J==0
-        if (col0 == 0) matrix[i][0] = 0;
-    }
-}
 };
