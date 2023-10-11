a66a112e6782c3c5e665f030acb5cbfa72d1aeea
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-10-11 23:02:36 +0530

    Time: 4 ms (40.89%), Space: 7.5 MB (16.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        
-        int ans[n][n];
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+    
+    // IN EXTRA SPACE
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         int ans[n][n];
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
                 
-                ans[j][n-i-1] = matrix[i][j];
-                
-            }
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                
-                matrix[i][j]= ans[i][j];
-                
-            }
-        }
-    }
+//                 ans[j][n-i-1] = matrix[i][j];           
+//             }
+//         }
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){         
+//                 matrix[i][j]= ans[i][j];        
+//             }
+//         }
+//     }
+    
+    // IN O(1) SPACE
+    // take transpose then reverse every row
+    
+     void rotate(vector<vector<int>>& matrix) {
+         int n = matrix.size();
+         //taking transpose
+         for(int i=0; i<=n-2; i++){
+             for(int j=i+1; j<=n-1; j++){
+                 swap(matrix[i][j], matrix[j][i]);
+             }
+         }
+         
+         //reversing all rows
+         for(int i=0; i<n; i++){
+             reverse(matrix[i].begin(), matrix[i].end());
+         }
+     }
 };
