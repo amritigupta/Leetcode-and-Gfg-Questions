e79e2c12538e088c311fb3a2383e405e1a6f7f52
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-12-21 01:31:58 +0530

    Time: 4 ms (22.49%), Space: 6.5 MB (22.97%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -5,6 +5,12 @@ public:
     bool isPowerOfFour(int n) {
         
         double f = log2(n);
+        
+        // log 16 = 4
+        // log 20 = 
+        // log 4 = 2
+        // log 64 = 6
+        
         if(n<4){
             if(n==1){
                 return true;
@@ -13,7 +19,8 @@ public:
                 return false;
             }
         }
-        if(int(f)%2==0 && f==ceil(f) && f==floor(f) && (n&(n-1))==0){
+        
+        if((n&(n-1))==0 && int(f)%2==0  ){
             return 1;
         }
         else{
