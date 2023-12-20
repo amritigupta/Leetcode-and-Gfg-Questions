67444f23eaf510ccb173e9be368145fca54512b9
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2023-12-21 02:08:30 +0530

    Time: 12 ms (20.25%), Space: 11.3 MB (9.16%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -5,8 +5,9 @@ public:
         int count=0;
         while(n>0)
         {
-            count++;
-            n=n&n-1;
+            int bit = n&1;
+            if(bit) count++;
+            n=n>>1;
         }
         return count;
     }
