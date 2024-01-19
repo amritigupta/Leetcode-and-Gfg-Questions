f27ef16b884d65ec0fa66e0bf3b9db8598b652e1
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-01-20 01:29:50 +0530

    Time: 5 ms (33.77%), Space: 11.8 MB (80.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -47,12 +47,13 @@ class Solution {
  public:
   vector<string> generateParenthesis(int n) {
     vector<string> ans;
-    dfs(n, n, "", ans);
+      string s = "";
+    dfs(n, n, s, ans);
     return ans;
   }
 
  private:
-  void dfs(int l, int r, string&& path, vector<string>& ans) {
+  void dfs(int l, int r, string &path, vector<string>& ans) {
     if (l == 0 && r == 0) {
       ans.push_back(path);
       return;
@@ -60,12 +61,12 @@ class Solution {
 
     if (l > 0) {
       path.push_back('(');
-      dfs(l - 1, r, move(path), ans);
+      dfs(l - 1, r, path, ans);
       path.pop_back();
     }
     if (l < r) {
       path.push_back(')');
-      dfs(l, r - 1, move(path), ans);
+      dfs(l, r - 1, path, ans);
       path.pop_back();
     }
   }
