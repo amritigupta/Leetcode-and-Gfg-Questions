0921488d258c176842d8e88dd4ac698edaf2754d
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-08-14 00:45:33 +0530

    Time: 6 ms (38.87%), Space: 12.9 MB (58.11%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,33 @@
 class Solution {
-    void f(int ind, int target, vector<int>& arr,  vector<vector<int>> &ans, vector<int> &ds, int &n){
-        if(target==0){
-            ans.push_back(ds);
+
+    void f(int idx,vector<int>& candidates, int target,  vector<vector<int>> &ans, vector<int> &v){
+        if(target<0){
             return;
         }
-        
-        for(int i=ind; i<n; i++){
-            if(arr[i]>target){ //arr[i] > target
-                break;
-            }
-            
-            if(i>ind && arr[i]==arr[i-1]){ //dont take duplicates
+        if(target==0){
+            ans.push_back(v);
+            return;
+        }
+        if(idx==candidates.size()){
+            return;
+        }
+
+        for(int i=idx; i<candidates.size(); i++){
+            if(i!=idx && candidates[i]==candidates[i-1]){
                 continue;
             }
-            
-            ds.push_back(arr[i]);
-            f(i+1, target-arr[i], arr, ans, ds, n);
-            ds.pop_back();
+            v.push_back(candidates[i]);
+            f(i+1,candidates,target-candidates[i],ans,v );
+            v.pop_back();
         }
+
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
-        vector<int> ds;
-        int n = candidates.size();
-        f(0, target, candidates, ans, ds, n);
+        vector<int> v;
+        f( 0, candidates, target, ans , v);
         return ans;
     }
 };
