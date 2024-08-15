8b132772d1d76263887a8a2d4d8e64bd1a332fb4
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-08-16 03:25:01 +0530

    Time: 11 ms (57.65%), Space: 14 MB (61.98%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,49 +1,68 @@
 class Solution {
-   void dfs(int m,int n,int row,int col,vector<vector<char>>& board,vector<vector<int>> &vis,int delrow[],int delcol[]){
-        vis[row][col]=1;
-        //neighbours
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            //validation
-            if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && board[nrow][ncol]=='O' && vis[nrow][ncol]==0)
-                dfs(m,n,nrow,ncol,board,vis,delrow,delcol);
-        }
-        
-    }
+    vector<int> di = {-1,1,0,0};
+    vector<int> dj = {0,0,-1,1};
+
 public:
     void solve(vector<vector<char>>& board) {
-        int m =board.size();
-        int n =board[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        int delrow[]={-1,1,0,0};
-        int delcol[]={0,0,-1,1};
-        // l&r
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O' && vis[i][0]==0)
-                dfs(m,n,i,0,board,vis,delrow,delcol);
-            
-             if(board[i][n-1]=='O' && vis[i][n-1]==0)
-                dfs(m,n,i,n-1,board,vis,delrow,delcol);
-            
-        }
-        
-        //u&d
-        for(int j=0;j<n;j++){
-            if(board[0][j]=='O' && vis[0][j]==0)
-                dfs(m,n,0,j,board,vis,delrow,delcol);
-            
-             if(board[m-1][j]=='O' && vis[m-1][j]==0)
-                dfs(m,n,m-1,j,board,vis,delrow,delcol);
-        }
-        
-            //fill remaining O's with X
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='O' && vis[i][j]==0)
-                    board[i][j]='X';
+        int n = board.size();
+        int m = board[0].size();
+
+        queue<pair<int,int>> q;
+
+        for(int i=0; i<n; i++){
+            if(board[i][0]=='O'){
+                q.push({i, 0});
+            }
+            if(board[i][m-1]=='O'){
+                q.push({i, m-1});
             }
         }
-        
+
+        for(int j=1; j<m-1; j++){
+            if(board[0][j]=='O'){
+                q.push({0,j});
+            }
+            if(board[n-1][j]=='O'){
+                q.push({n-1, j});
+            }
+        }
+
+
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                int i = q.front().first;
+                int j = q.front().second;
+                board[i][j]='.'; //this marks it as visited O on boundary or connnected to such O
+                q.pop();
+
+                for(int k=0; k<4; k++){
+                    int ni = i+di[k];
+                    int nj = j+dj[k];
+
+                    if(ni>=0 && ni<n && nj>=0 && nj<m && board[ni][nj]=='O'){
+                        q.push({ni,nj});
+                    }
+                }
+            }
+        }
+
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]=='.'){
+                    board[i][j]='O';
+                }
+            }
+        }
+
+        return;
     }
 };
