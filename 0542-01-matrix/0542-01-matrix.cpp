a975423bfc327f77ca89e8a952d9b10afee55d54
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-08-16 02:49:15 +0530

    Time: 219 ms (5.03%), Space: 34.2 MB (79.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,66 +1,48 @@
 class Solution {
+    vector<int> di = {-1,1,0,0};
+    vector<int> dj = {0,0,-1,1};
+
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-       
-        int drow[]={0,-1,0,1};
-        int dcol[]={-1,0,1,0};
-        
-        vector<vector<int>> dist(n, vector<int> (m,1e5));
-        queue<pair< pair<int,int> , int >> q;
-        
-        for (int i=0; i<n; i++){
+
+        queue<pair<int,int>> q;
+        vector<vector<int>> dist(n, vector<int>(m, 1e6));
+
+        for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(mat[i][j]==0){
                     dist[i][j]=0;
-                    q.push({{i,j},0});
+                    q.push({i, j});
                 }
             }
         }
-        
+
+        int steps = 0;
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dis = q.front().second;
-            q.pop();
-            for (int i=0; i<4;i++){
-                int nrow = row+drow[i];
-                int ncol = col+dcol[i];
-                
-                if (nrow>=0 && nrow<n && ncol>=0 && ncol<m && dist[nrow][ncol]>dis+1){
-                    dist[nrow][ncol]=dis+1;
-                    q.push({{nrow,ncol},dis+1});
+            int sz = q.size();
+            steps++;
+            for(int l=0; l<sz; l++){
+                int i = q.front().first;
+                int j = q.front().second;
+                cout<<i<<" "<<j<<endl;
+                q.pop();
+
+                for(int k=0; k<4; k++){
+                    int ni = i+di[k];
+                    int nj = j+dj[k];
+                    // if(ni>=0 && ni<n && nj>=0 && nj<m) cout<<ni<<" "<<nj<<" "<<dist[ni][nj]<<endl;
+                    if(ni>=0 && ni<n && nj>=0 && nj<m && dist[ni][nj]==1e6){
+                        dist[ni][nj]=steps;
+                        q.push({ni,nj});
+                    }
                 }
+
             }
         }
-            
-        
+
         return dist;
+
     }
-    
-    
-//     // METHOD 2
-//     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-//         int m = mat.size(), n = mat[0].size();
-//         queue<pair<int, int>> q;
-//         for (int r = 0; r < m; ++r)
-//             for (int c = 0; c < n; ++c)
-//                 if (mat[r][c] == 0) q.push({r, c});
-//                 else mat[r][c] = -1; // Marked as not processed yet!
-           
-//         int dr[]={0,-1,0,1};
-//         int dc[]={-1,0,1,0};
-//         while (!q.empty()) {
-//             auto [r, c] = q.front(); 
-//             q.pop();
-//             for (int i = 0; i < 4; ++i) {
-//                 int nr = r + dr[i], nc = c + dc[i];
-//                 if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
-//                 mat[nr][nc] = mat[r][c] + 1;
-//                 q.push({nr, nc});
-//             }
-//         }
-//         return mat;
-//     }
 };
