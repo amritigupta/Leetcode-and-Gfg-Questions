5fdd10b56242721696eed28ff4420052bb5cc0f4
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-08-25 23:50:20 +0530

    Time: 182 ms (70.75%), Space: 108.8 MB (74.4%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -1,29 +1,96 @@
 class Solution {
-    long long f(int row, int col, vector<vector<int>>& points, int n, int m, vector<vector<long long>> &dp){
+    // MEMOIZATION
 
-        if(dp[row][col]!=-1) return dp[row][col];
+    // long long f(int row, int col, vector<vector<int>>& points, int n, int m, vector<vector<long long>> &dp){
+    //     if(row==n){
+    //         return 0;
+    //     }
 
-        long long pts = points[row][col];
+    //     if(dp[row][col]!=-1) return dp[row][col];
 
-        long long ans = pts;
-        for(int i=0; i<m; i++){
-           if(row+1<n) ans = max(ans,pts+f(row+1, i, points, n, m, dp) - abs(i-col));
-        }
+    //     long long pts = points[row][col];
 
-        return dp[row][col]=ans;
-    }
+    //     long long ans = pts;
+    //     for(int i=0; i<m; i++){
+    //         ans = max(ans,pts+f(row+1, i, points, n, m, dp) - abs(i-col));
+    //     }
+
+    //     return dp[row][col]=ans;
+    // }
 public:
+    // long long maxPoints(vector<vector<int>>& points) {
+    // //     long long ans = 0;
+    // //     int n = points.size();
+    // //     int m = points[0].size();
+
+    // //     vector<vector<long long>> dp(n, vector<long long>(m, -1));
+
+    // //     for(int i=0; i<m; i++){
+    // //         ans=max(ans, f(0, i, points, n, m, dp));
+    // //     }
+
+    // //     return ans;
+    // // }
+
+
+    // // TABULATION 
+
+    // long long maxPoints(vector<vector<int>>& points) {
+    //     long long ans = 0;
+    //     int n = points.size();
+    //     int m = points[0].size();
+
+    //     vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+
+    //     for(int i=n-1; i>=0; i--){ //row
+    //         for(int j=m-1; j>=0; j--){ //col
+    //             long long pts = points[i][j];
+    //             long long temp = pts;
+    //             for(int k=0; k<m; k++){ // i
+    //                 temp = max(temp, pts+dp[i+1][k] - abs(k-j));
+    //             }
+    //             dp[i][j]=temp;
+    //         }
+    //     }
+
+    //     for(int i=0; i<m; i++){
+    //         ans=max(ans, dp[0][i]);
+    //     }
+
+    //     return ans;
+    // }
+
+
+    // optimized tabulation to remove TLE
+
     long long maxPoints(vector<vector<int>>& points) {
-        long long ans = 0;
         int n = points.size();
         int m = points[0].size();
 
-        vector<vector<long long>> dp(n, vector<long long>(m, -1));
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+        vector<long long> leftmax(m+1, 0);
+        vector<long long> rightmax(m+1, 0);
+        for(int row=n-1; row>=0; row--){
+            leftmax[0]=dp[row+1][0];
+            for(int col=1; col<m; col++){
+                leftmax[col]= max(dp[row+1][col], leftmax[col-1]-1);
+            }
 
-        for(int i=0; i<m; i++){
-            ans=max(ans, f(0, i, points, n, m, dp));
+            rightmax[m-1]=dp[row+1][m-1];
+            for(int col=m-2; col>=0; col--){
+                rightmax[col]=max(dp[row+1][col], rightmax[col+1]-1);
+            }
+
+            for(int col=0; col<m; col++){
+                dp[row][col]= max(leftmax[col], rightmax[col])+points[row][col];
+            }
         }
 
+        long long ans = 0;
+        for(int i=0; i<m ; i++){
+            ans=max(ans , dp[0][i]);
+        }
         return ans;
+
     }
 };
