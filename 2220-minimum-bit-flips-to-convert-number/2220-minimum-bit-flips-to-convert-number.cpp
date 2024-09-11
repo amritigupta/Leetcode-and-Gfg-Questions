87e1d5f70930b21f3d8004e6aa823335be6403ff
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-09-11 22:06:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (44.87%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorr = start ^ goal;
+        int cnt = 0;
+        while(xorr!=0){
+            if(xorr&1==1){
+                cnt++;
+            }
+            xorr>>=1;
+        }
+        return cnt;
+    }
+};
