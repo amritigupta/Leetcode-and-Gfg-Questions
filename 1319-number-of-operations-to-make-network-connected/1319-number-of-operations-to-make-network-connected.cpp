5df452341660857bb294b0e589c9c0998e666f4a
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-09-11 22:13:12 +0530

    Time: 148 ms (5.24%), Space: 59.2 MB (9.82%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,40 +1,31 @@
 class Solution {
-    vector<int>parent;
-    int find(int x) {
-        if(parent[x]==x){
-            return x;
-        }
-        return parent[x] = find(parent[x]);
+private:
+    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
+    {
+        visited[src] = true;
+        for(int i : adj[src])
+            if(!visited[i])
+                dfs(adj, visited, i);
     }
-    
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         
-        int c = connections.size();
-        if (n-1 > c) return -1;
-        int extra=0;
-        
-        parent.resize(n);
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-        
-        for (int i = 0; i < c; i++) {
-            int x = find(connections[i][0]);
-            int y = find(connections[i][1]);
-            if (x != y) parent[y] = x;
-            if(x==y) extra++; //same parent 
-        }
-        
-        
-        // ans stores number of connected components 
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-            if (parent[i] == i)
-                ans++;
-        
-        if(extra>=ans-1) return ans-1;
-        else{
+        if(connections.size() < n - 1)
             return -1;
+        vector<vector<int>> adj(n);
+        for(auto v : connections)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
         }
+        vector<bool> visited(n, false);
+        int components = 0;
+        for(int i=0; i<n; i++)
+            if(!visited[i])
+            {
+                dfs(adj, visited, i);
+                components++;
+            }
+        return components - 1;
     }
 };
