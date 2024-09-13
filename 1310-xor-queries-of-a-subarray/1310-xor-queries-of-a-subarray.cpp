b2825328332dc53157da6772e270021aefb935ce
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-09-14 01:14:22 +0530

    Time: 56 ms (74.42%), Space: 41.9 MB (60.54%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n = arr.size();
-        int prefix[n];
-        prefix[0]=arr[0];
-        for(int i=1; i<n ; i++){
-            prefix[i]=prefix[i-1]^arr[i];
+        vector<int> ans;
+
+        vector<int> pre(n);
+        pre[0] = arr[0];
+        // cout<<pre[0]<<" ";
+
+        for(int i=1;i<n;i++){
+            pre[i] = pre[i-1] ^ arr[i];
+            // cout<<pre[i]<<" ";
         }
-        vector<int> res;
-        for(int i=0; i<queries.size(); i++){
-            int L = queries[i][0];
-            int R = queries[i][1];
-            
-            if(L==0){
-                res.push_back(prefix[R]);
+        // cout<<endl;
+
+        for(auto& val:queries){
+            int l = val[0];
+            int r = val[1];
+
+            if(l==0){
+                ans.push_back(pre[r]);
+                continue;
             }
-            else{
-                res.push_back(prefix[R]^prefix[L-1]);
-            }
+
+            int calc = pre[r]^pre[l-1];
+            ans.push_back(calc);
         }
-        return res;
+
+        return ans;
     }
 };
