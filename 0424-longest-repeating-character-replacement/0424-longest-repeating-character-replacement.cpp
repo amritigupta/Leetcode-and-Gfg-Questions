94652ae9d909555908f27a8534653641369f3440
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-11-07 12:28:12 +0530

    Time: 8 ms (39.4%), Space: 8.2 MB (98.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,37 @@
-
 class Solution {
 public:
-
-    
     int characterReplacement(string s, int k) {
-        vector<int> count(26, 0);
-        int max_len = 0,  max_majority_occur_till_now = 0, start = 0;
-        for (int end = 0; end < s.size(); end++) {
-            char ch = s[end] - 'A';
-            count[ch]++;
-             max_majority_occur_till_now = max( max_majority_occur_till_now, count[ch]);
-            
-            if (end - start - max_majority_occur_till_now + 1 > k) {
-                count[s[start] - 'A']--;
-                start++;
+        int i=0, j=0;
+        int n = s.size();
+        int maxi=0;
+        int maxcharcnt = 0;
+        vector<int> freq(27,0);
+        while(j<n){ //o(n)
+            freq[s[j]-'A']++;
+            maxcharcnt = max(maxcharcnt, freq[s[j]-'A']);
+            while((j-i+1 - maxcharcnt)>k){ //i runs from 0 to N o(n)
+                freq[s[i]-'A']--;
+                maxcharcnt=0;
+                // THIS UPDATION IS OPTIONAL (EXPLAINED BELOW)
+                for(int p=0; p<=26; p++){ //o(26) == o(1)
+                    maxcharcnt = max(maxcharcnt, freq[p]);
+                }
+                i++;
             }
-            max_len = end - start + 1;
+            maxi = max(maxi, j-i+1);
+            j++;
         }
-        return max_len;
-    
+
+        return maxi;
     }
+
+    //optimizing
+    // WHEN WE RUN THE O(26) LOOP TO UPDATE MAXCHARCNT
+    // WE KNOW THE MAXCHARCNT WILL OBVIOUSLY DECREASE FROM THE PREVIOUS MAXCHARCNT
+    //
+    // observe that maxlen(j-i+1) - maxcharcnt should be <= K right
+    // so what happens when u want to increase ur maxlen
+    // u will want to increase maxcharcnt
+    // but in the o(26) loop we wud be decreasing maxcharcnt which eventually wouldnt give us an answer bigger than our current maxlen
+    // so we can just not update it to reduce from O(2N*26) TO O(2N)
 };
