8fc20b319c5ede7788ac47032338ac75675d8682
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-11-07 12:38:19 +0530

    Time: 19 ms (53.56%), Space: 10.4 MB (73.71%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
+    string minWindow(string s, string t) {
+        unordered_map<int,int> mpp;
+        for(auto it: t){
+            mpp[it]++;
+        }
 
-     string minWindow(string s, string t) {
-         unordered_map <char, int> mpp;
-         for(auto it: t){
-             mpp[it]++;
-         }
-         int count = mpp.size();
-         int i=0;
-         int j=0;
-         int ans = INT_MAX;
-         int start=0;
-         
-         while(j<s.length()){
-             mpp[s[j]]--;
-             
-             if(mpp[s[j]]==0){
-                 count--;
-             }
-             
-             if(count==0){
-               while(count==0){
-                 if(ans>j-i+1){
-                     ans = j-i+1;
-                     start = i;
-                 }
-                 
-                 mpp[s[i]]++;
-                 if(mpp[s[i]]>0)
-                     count++;
-             
-                 i++;
-               }
-             }
-             j++;
-         }
-         
-         if(ans!=INT_MAX)
-             return s.substr(start,ans);
-         else{
-             return "";
-         }
-     }
+        // TTA
+
+        // TOMTTA
+
+        int n = s.size();
+        int count = mpp.size();
+        int i=0;
+        int j=0;
+        int ans=INT_MAX;
+        int start=0;
+
+        while(j<n && i<n){
+           if(mpp.find(s[j]) != mpp.end()){
+                mpp[s[j]]--;
+
+                if(mpp[s[j]]==0)
+                    count--;
+            }
+            if(count==0){
+                while(count==0 && i<n){
+                    if(ans>(j-i+1)){
+                        ans = j-i+1;
+                        start= i;
+                    }
+
+                    if(mpp.find(s[i])!=mpp.end()){
+                        mpp[s[i]]++;
+                        if(mpp[s[i]]>0){
+                            count++;
+                        }
+                    }
+
+                    i++;
+                }
+            }
+
+            j++;
+        }
+
+        if(ans!=INT_MAX){
+            return s.substr(start,ans);
+        }
+        else{
+            return "";
+        }
+    }
 };
