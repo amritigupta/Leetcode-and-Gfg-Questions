53cb3547b6c29d5822624b0e6c19347138723b5e
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-11-12 17:29:42 +0530

    Time: 3 ms (32.54%), Space: 15.7 MB (99.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,50 +1,47 @@
 class Solution {
+    vector<int> dx = {-1,1,0,0};
+    vector<int> dy = {0,0,1,-1};
+
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        
-        int count=0;
-        queue< pair<pair<int,int>,int> > q;
-        int dr[]={-1,1,0,0};
-        int dc[]={0,0,1,-1};
-        int oranges=0;
-   
+
+        queue<pair<int,int>> q;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j]!=0){
-                    oranges++;
-                }
                 if(grid[i][j]==2){
-                    count++;
-                    q.push({{i,j},0});
+                    q.push({i,j});
                 }
             }
         }
-        
-        int maxi=0;
+
+        int ans = -1;
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int time = q.front().second;
-            maxi=max(time,maxi);
-            q.pop();
-            
-            for(int k=0; k<4; k++){
-                int newr = r+dr[k];
-                int newc = c+dc[k];
-                
-                if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]==1 ){
-                    count++;
-                    grid[newr][newc]=2;
-                    q.push({{newr,newc},time+1});
+            int sz = q.size();
+            ans++;
+            for(int i=0; i<sz; i++){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                for(int i=0; i<4; i++){
+                    int nx = x+dx[i];
+                    int ny = y+dy[i];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        q.push({nx,ny});
+                    }
                 }
             }
         }
-        
-        if(count==oranges){
-            return maxi;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
         }
-        else return -1;
+        if(ans==-1) return 0;
+        return ans;
     }
 };
