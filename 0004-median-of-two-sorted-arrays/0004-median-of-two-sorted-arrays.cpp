7d6df5e913d4aaab24b371e9c5ec9aab897bbc22
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2024-11-29 10:26:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,38 @@
+// Optimized Using: Two Pointer with Extra Space
+  // Time Complexity: O(m+n)
+  // Space Complexity: O(m+n)
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
         
-        if(n>m){
-            return findMedianSortedArrays(nums2,nums1); //making sure nums1 is always smaller array
+        // Create a single sorted by merging two sorted arrays
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i=0;
+        int j=0;
+        int lastindex=-1;
+             
+        // Initialize a new array
+           vector<int>v(n1+n2,0);
+        
+        while(i<n1&&j<n2)
+        {
+            if(nums1[i]<=nums2[j])
+                v[++lastindex]=nums1[i++];
+            else
+                v[++lastindex]=nums2[j++];
         }
         
-        int low=max(0, (n+m+1)/2 - m) ,high=min(n, (n+m+1)/2 ); //n is max elements u can take from nums1 array
-        int left=(n+m+1)/2; 
-        while(low<=high){
-            int mid1 = (low+high)/2; //1 //elements taken from nums1
-            int mid2= left-mid1;   // 1  //elements taken from nums2
-            int l1=INT_MIN, l2 = INT_MIN;
-            int r1 = INT_MAX, r2=INT_MAX;
-            
-            if(mid1 < n){
-                r1 = nums1[mid1]; //3 
-            }
-            if(mid2 < m ){
-                r2 = nums2[mid2]; //INT_MAX
-            }
-            if(mid1-1>=0){
-                l1 = nums1[mid1-1]; // 1
-            }
-            if(mid2-1>=0){
-                l2 = nums2[mid2-1]; // 2
-            }
-            
-            if (l2<=r1 && l1<=r2){
-                if((n+m)%2==1){
-                    return (max(l1, l2));
-                }
-                else return double((max(l1,l2)+min(r1,r2))/2.0);
-            }
-            
-            if(l1>r2){
-                high = mid1-1;
-            }
-            else{
-                low=mid1+1;
-            }
-        }
-        return 0;
+        while(i<n1)
+            v[++lastindex]=nums1[i++];
+        while(j<n2)
+            v[++lastindex]=nums2[j++];
+        
+    // Return the result
+        int n=n1+n2;
+        return n%2?v[n/2]:(v[n/2]+v[n/2-1])/2.0;
+        
     }
 };
+
+** Accepted **
