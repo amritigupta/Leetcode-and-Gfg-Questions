cf82111fa383a261905201c46a79caf243f9d9bf
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-01-14 17:11:25 +0530

    Time: 4 ms (12.71%), Space: 74.6 MB (5.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,33 +1,26 @@
-
 class Solution {
 public:
-//     int maxSubArray(vector<int>& nums) {
-//         int start = 0, sum = 0, max = INT_MIN, n = nums.size();
-        
-//         for(int end = 0; end < n; end++) {
-//             sum += nums[end];   
-//             if(sum > max) max = sum;
-//             while(sum < 0) 
-//                 sum -= nums[start++];
-//         }
-        
-//         return max;
-//     }
-    
-     int maxSubArray(vector<int>& nums) {
-         int n = nums.size();
-         int sum=0;
-         int i=0;
-         int maxi = INT_MIN;
-         while(i<n){
-             sum+=nums[i];
-             maxi=max(maxi,sum);
-             if(sum<0){
-                 sum=0;
-             }
-             i++;
-         }
-         
-         return maxi;
-     }
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+
+        int sum=0;
+        vector<int> pre(n);
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            pre[i]=sum;
+        }
+        int ans = sum; 
+        int minval=1e4;
+
+        for(int i=0; i<pre.size(); i++){
+
+            if(nums[i]>0)ans=max(ans, max(pre[i]-minval, pre[i]));
+            else ans=max(ans, nums[i]);
+
+            minval=min(minval, pre[i]);
+        }
+
+        return ans;
+    }
 };
