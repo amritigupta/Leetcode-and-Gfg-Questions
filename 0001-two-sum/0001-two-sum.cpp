922d93af54def634e1aaa8ad97350ae4d21b70a5
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-01-14 16:36:20 +0530

    Time: 13 ms (45.98%), Space: 18.2 MB (7.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,35 @@
-
-
 class Solution {
 public:
-    vector<int> twoSum(vector<int> a, int target) 
-    {
-        unordered_map<int, int> mpp;
-
-        vector <int> ans;
-        int N = a.size();
-        
-        for (int i=0; i<N; i++){
-            if (mpp.find(target - a[i])!=mpp.end()){
-                ans.push_back(i);
-                ans.push_back(mpp[target-a[i]]);
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        unordered_map<int, vector<int>> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]].push_back(i);
+        }
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(nums[i]+nums[j]==target){
+                if(nums[i]==nums[j]){
+                    v.push_back(mpp[nums[i]][0]);
+                    v.push_back(mpp[nums[i]][1]);
+                }
+                else{
+                  v.push_back(mpp[nums[i]][0]);
+                  v.push_back(mpp[nums[j]][0]);
+                }
+                return v;
+            }
+            else if(nums[i]+nums[j]>target){
+                j--;
             }
             else{
-                mpp.insert(make_pair(a[i],i));
+                i++;
             }
         }
 
-        return ans;
+        return v;
     }
-}; 
-
+};
