c41026a98748ae50d2413bc1bf904ffe4de43f37
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-02-01 04:37:40 +0530

    Time: 123 ms (5.14%), Space: 18 MB (94.96%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,70 +1,29 @@
 class Solution {
-    vector<int> size;
-    vector<int> parent;
 public:
-    int findupar(int v){
-        if(parent[v]==v) return v;
-        else return parent[v]=findupar(parent[v]);
+    bool valid(vector<int>& p1 , vector<int>& p2){
+        if(p1[0] == p2[0]) return true;
+        if(p1[1] == p2[1]) return true;
+        return false;
     }
-
-    void Union(int i, int j){
-        int upri=findupar(i);
-        int uprj=findupar(j);
-        if(upri==uprj) return;
-        else{
-            if(size[upri]<size[uprj]){
-                parent[upri]=uprj;
-                size[uprj]+=size[upri];
-            }
-            else{
-                parent[uprj]=upri;
-                size[upri]+=size[uprj];                
+    void dfs(vector<vector<int>>& stones , vector<bool>& visited , int i){
+        visited[i] = true;
+        for(int j = 0 ; j < stones.size() ; j++){
+            if(valid(stones[i] , stones[j]) && !visited[j]) {
+                dfs(stones , visited , j);
             }
         }
     }
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        parent.resize(n);
-        size.resize(n);
-        
-        for(int i=0; i<n; i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-
-        unordered_map<int,vector<int>> row2stone;
-        unordered_map<int,vector<int>> col2stone;
-
-        for(int i=0; i<n; i++){
-            row2stone[stones[i][0]].push_back(i);
-            col2stone[stones[i][1]].push_back(i);
-        }
-
-        for(auto it: row2stone){
-            vector<int> x = it.second;
-            int u = x[0];
-            for(int i=1; i<x.size(); i++){
-                int v = x[i];
-                Union(u, v);
+        vector<bool> visited(n , false);
+        int count = 0;
+        for(int i = 0; i < n ;i++){
+            if(!visited[i]){
+                dfs(stones , visited , i);
+                count++;
             }
         }
 
-        for(auto it: col2stone){
-            vector<int> x = it.second;
-            int u = x[0];
-            for(int i=1; i<x.size(); i++){
-                int v = x[i];
-                Union(u, v);
-            }
-        }
-
-        int comp=0;
-
-        for(int i=0; i<n; i++){
-            if(findupar(i)==i) comp++;
-        }
-
-        return n-comp;
-
+        return n - count;
     }
 };
