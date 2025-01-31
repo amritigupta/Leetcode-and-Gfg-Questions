e42e4acbf1603fd218956a3a3a1c5f1e02b05e3c
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-02-01 01:02:54 +0530

    Time: 93 ms (16.61%), Space: 44.9 MB (35.04%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,93 +1,125 @@
 // DSU HELPS IN DYNAMIC MERGING/CONNECTING
 
-class DisjointSet{
-    
+class DisjointSet {
+	vector<int> rank, parent;
 public:
-    vector <int> parent,size;
-    
-    DisjointSet(int n){
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i < n; i++)
-        {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
+	DisjointSet(int n) {
+		rank.resize(n + 1, 0);
+		parent.resize(n + 1);
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+		}
+	}
 
-    int findUltimateParent(int node){
-        if (node == parent[node]){
-            return node;
-        }
+	int findupar(int node) {
+		if (node == parent[node]) {
+			return node;
+		}
 
-        return parent[node] = findUltimateParent(parent[node]);
-    }
+		return parent[node] = findupar(parent[node]); //path compression
+	}
 
-    void unionBySize(int u, int v){
-        int ulp_u = findUltimateParent(u);
-        int ulp_v = findUltimateParent(v);
+	void unionbyrank(int u, int v) {
+		int ulp_u = findupar(u);
+		int ulp_v = findupar(v);
+		if (ulp_u == ulp_v) {
+			// u and v belong to same component
+			return;
+		}
 
-        if(ulp_u==ulp_v){
-            return;
-        }     
-
-        if(size[ulp_u]< size[ulp_v])  {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-
+		if (rank[ulp_u] < rank[ulp_v]) {
+			//smaller gets attached to larger
+			//rank doesnt change
+			parent[ulp_u] = ulp_v;
+		}
+		else if (rank[ulp_u] > rank[ulp_v]) {
+			//smaller gets attached to larger
+			//rank doesnt change
+			parent[ulp_v] = ulp_u;
+		}
+		else {
+			//attach v to u (or vice versa)
+			//incease rank of u by 1 (or vice versa)
+			parent[ulp_v] = ulp_u;
+			rank[ulp_u]++;
+		}
+	}
 };
 
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& details) {
-        
-        int n = details.size();
+    vector<vector<string>> accountsMerge(vector<vector<string>> &accounts) {
+        int n = accounts.size();
         DisjointSet ds(n);
+        // cout<<n<<endl;
         unordered_map <string, int> mapmailnode;
         
-        for(int i=0; i<n; i++){
-            for(int j=1; j<details[i].size(); j++){
-                string mail = details[i][j];
-                if(mapmailnode.find(mail) == mapmailnode.end()){
-                    mapmailnode[mail]=i;
+        for(int i=0; i<accounts.size(); i++){
+            for(int j=1 ; j<accounts[i].size(); j++){
+                if(mapmailnode.find(accounts[i][j])==mapmailnode.end()){
+                    mapmailnode[accounts[i][j]] = i;
+                    // cout<<accounts[i][j]<<" "<<i<<endl;
                 }
                 else{
-                    ds.unionBySize(i, mapmailnode[mail]); //map current node and mapmailnode[mail]
+                    ds.unionbyrank(mapmailnode[accounts[i][j]], i);
                 }
-          }
+            }
         }
         
-        vector<string> mergedmail[n];
-        for(auto it: mapmailnode){
-            string mail = it.first;
-            int node = ds.findUltimateParent(it.second);
-            mergedmail[node].push_back(mail);
-        }
-        
-        vector<vector<string>>  ans;
+
+        unordered_map <int, set<string>> mpp;
+
         for(int i=0; i<n; i++){
-            if(mergedmail[i].size()==0){
-                continue;
+            cout<<i<<" "<<ds.findupar(i)<<endl;
+            if(ds.findupar(i)==i){
+                if(mpp.find(i)==mpp.end()){
+                  set<string> s;
+                  for(int j=1; j<accounts[i].size(); j++){
+                    s.insert(accounts[i][j]);
+                  }
+                  mpp[i]=s;
+                }
+                else{
+                  for(int k=1; k<accounts[i].size(); k++){
+                    mpp[i].insert(accounts[i][k]);
+                    // john's map is added w john00
+                   }     
+                }
+                // john is matched to johnsmith and john_newyork
+                // mary with mary
+                //john with johnnybravo
             }
-            
-            sort(mergedmail[i].begin(), mergedmail[i].end());
-            
-            vector<string> temp;
-            temp.push_back(details[i][0]);
-            for(auto it: mergedmail[i]){
-                temp.push_back(it);
+            else{
+                int ultimateparent = ds.findupar(i); // 1 -> 0
+
+                if(mpp.find(ultimateparent)!=mpp.end()){
+                  for(int k=1; k<accounts[i].size(); k++){
+                    mpp[ultimateparent].insert(accounts[i][k]);
+                    // john's map is added w john00
+                   }
+                }
+                else{
+                  set<string> s;
+                  for(int j=1; j<accounts[i].size(); j++){
+                    s.insert(accounts[i][j]);
+                  }
+                  mpp[ultimateparent]=s; 
+                }
             }
-            
-            ans.push_back(temp);
         }
+        
+        vector<vector<string>> ans;
+        for(auto it: mpp){
+            vector<string> s;
+            s.push_back(accounts[it.first][0]);
+            for(auto it2: it.second){
+                s.push_back(it2);
+            }
+            ans.push_back(s);
+        }
+        
         return ans;
+        
     }
-    
  
 };
