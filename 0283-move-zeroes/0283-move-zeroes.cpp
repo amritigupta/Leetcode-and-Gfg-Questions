53856a1f58cc24a79306a95b6be2bcc5ee391431
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-02-06 03:55:46 +0530

    Time: 0 ms (100%), Space: 23.8 MB (50.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-   void moveZeroes(vector<int>& nums) {
-       int j=-1;
-       int n = nums.size();
-       for(int i=0; i<n; i++){
-           if(nums[i]==0){
-               j=i;
-               break;
-           }
-       }
-       
-       if(j==-1){
-           return; // no zeroes 
-       }
-       
-       for(int i=j+1; i<n; i++){
-           if(nums[i]!=0){
-               swap(nums[i], nums[j]);
-               j++; // j always points to 0 
-           }
-       }
-   }
+    void moveZeroes(vector<int>& nums) {
+        // 0 1 0 3 12
+        if(nums.size()==1) return;
+        int i=0;
+        int j=0;
+        int n = nums.size();
+        while(i<n){
+            if(nums[i]==0) break;
+            i++; // i = 1
+        }
+        while(j<n){
+            if(nums[j]!=0) break;
+            j++; // j = 0
+        }
+
+        while(i<n && j<n){
+            if(i<j) swap(nums[i],nums[j]);
+           
+
+            j++;
+            
+
+            while(j<n){
+                if(nums[j]!=0) break;
+                j++;
+            }
+            while(i<n){
+                if(nums[i]==0) break;
+                i++;
+            }
+        }
+
+    }
 };
