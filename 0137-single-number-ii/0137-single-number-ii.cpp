14fee51941a9e39d37ea4020773296a730a09acb
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-02-07 13:21:22 +0530

    Time: 0 ms (100%), Space: 13.5 MB (39.5%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
-        for(int bitindex=0; bitindex<=31; bitindex++){
-            int cnt=0;
-            // cout<<bitindex<<endl;
-            for(int i=0; i<n; i++){
-                // cout<<nums[i]<<" "<<(1<<bitindex)<<" ";
-                int x = nums[i]&(1<<bitindex);
-                // cout<<x<<endl;
-                if((nums[i]&(1<<bitindex))){
-                    cnt++;
-                }
-                
-            }
-            // cout<<endl<<bitindex<<" "<<cnt<<endl;
-            if(cnt%3!=0){
-                ans = ans|(1<<bitindex);
-            }
+        // k is 3
+        // m > logk
+        // m is 2
+
+        int x1=0;
+        int x2=0;
+
+        // bitwise operations count from 0 to 2^m-1
+        // 0 to 2^2-1 = 3
+        // k is also 3
+        // we need a mask then (as k is not less than 2^m-1)
+
+        // k = 3 --> 1 1 --> mask = ~(x1 & x2)
+        // if k was 5 --> 1 0 1 --> mask = ~(x1&(~x2)&x3)
+
+        for(auto i : nums){
+            x2^=(x1&i);
+            x1^=i;
+
+            int mask = ~(x1&x2);
+
+            x2&=mask;
+            x1&=mask;
         }
-        return ans;
+
+        return (x1|x2);
     }
 };
