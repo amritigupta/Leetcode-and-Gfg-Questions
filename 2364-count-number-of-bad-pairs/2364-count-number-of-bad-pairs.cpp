5ebc347d8a6fd00da5a602cb8a557cf10a708e7e
Amriti Gupta <106433545+amritigupta@users.noreply.github.com>
2025-02-09 19:14:51 +0530

    Time: 66 ms (70.36%), Space: 87.6 MB (81.46%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
-    long long countBadPairs(vector<int>& nums) {
+    // long long countBadPairs(vector<int>& nums) {
+    //     int n = nums.size();
+    //     unordered_map<int,int> mpp;
+    //     long long ans=0;
+    //     for(int i=0; i<n; i++){
+    //         mpp[nums[i]-i]++;
+    //         if((i+1)-mpp[nums[i]-i]<=0){
+    //             continue; //dont add anything
+    //         }
+    //         else{
+    //             ans+=(i+1-(mpp[nums[i]-i]));
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+    // another method
+    // total pairs = n*(n-1)/2 --> using nc2
+    // invalid = total - valid pairs 
+
+     long long countBadPairs(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int,int> mpp;
-        long long ans=0;
         for(int i=0; i<n; i++){
             mpp[nums[i]-i]++;
-            if((i+1)-mpp[nums[i]-i]<=0){
-                continue; //dont add anything
-            }
-            else{
-                ans+=(i+1-(mpp[nums[i]-i]));
-            }
         }
-        return ans;
-    }
+        long long tot = 1ll*n*(n-1)/2;
+        long long validcnt=0;
+        for(auto it: mpp){
+            //it.second c 2
+            validcnt+=1ll*(it.second-1)*(it.second)/2;
+        }
+
+        return tot-validcnt;
+     }   
 };
